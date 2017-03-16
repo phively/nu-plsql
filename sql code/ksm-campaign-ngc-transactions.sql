cc3874c6381b8728d54479e2f0b6b34e085fa0b3
Paul Hively <pbhively@gmail.com>
2017-03-16 16:01:28 -0500

    Commenting campaign transactions sql

--- sql code/ksm-campaign-ngc-transactions.sql
+++ sql code/ksm-campaign-ngc-transactions.sql
@@ -1,4 +1,11 @@
+/* See rpt_btaylor.nu_gft_t_ksm_dtl_curr for current counting method.
+   That table is updated by rpt_btaylor.wt099031_extracttransactions.
+
+Look into: receipt '0002275766' */
+
 With
+
+-- Transaction types table
 tms_trans As (
   (
     Select transaction_type_code, short_desc
@@ -13,6 +20,7 @@ tms_trans As (
 )
 
 (
+-- Gift and matching gift results from nu_gft_trp_gifttrans
 Select gft.id_number, gft.tx_number, gft.tx_gypm_ind, gft.transaction_type, tms_trans.short_desc,
   gft.date_of_record, gft.fiscal_year, gft.legal_amount, gft.pledge_status, gft.allocation_code,
   gft.alloc_short_name, gft.campaign_code
@@ -20,10 +28,11 @@ From nu_gft_trp_gifttrans gft
   Left Join tms_trans On tms_trans.transaction_type_code = gft.transaction_type
 Where gft.alloc_school = 'KM'
   And (gft.fiscal_year >= 2008 Or gft.campaign_code = 'CCR')
-  And gft.legal_amount > 0
-  And gft.tx_gypm_ind != 'Y'
-  And gft.allocation_code != 'BE'
-) Union All (
+  And gft.legal_amount > 0 -- Legal donors only
+  And gft.tx_gypm_ind In ('G', 'M')
+
+)/* Union All (
+-- Bequest and LE results
 Select plg.pledge_donor_id, plg.pledge_pledge_number, 'P' As gypm_ind, plg.pledge_pledge_type,
   tms_trans.short_desc, plg.pledge_date_of_record,
   Cast (ksm_pkg.get_fiscal_year(plg.pledge_date_of_record) As varchar(4)) As fiscal_year,
@@ -32,8 +41,31 @@ Select plg.pledge_donor_id, plg.pledge_pledge_number, 'P' As gypm_ind, plg.pledg
 From pledge plg
   Inner Join primary_pledge pp On plg.pledge_pledge_number = pp.prim_pledge_number
   Left Join tms_trans On plg.pledge_pledge_type = tms_trans.transaction_type_code
-Where plg.pledge_allocation_name = 'BE'
+Where plg.pledge_pledge_type In ('BE', 'LE')
   And plg.pledge_program_code = 'KM'
   And (ksm_pkg.get_fiscal_year(plg.pledge_date_of_record) >= 2008 Or plg.pledge_campaign = 'CCR')
   And plg.pledge_associated_code = 'P'
+  And plg.pledge_amount > 0 -- Legal donors only
+  And pp.discounted_amt > 0
+
+) Union All (
+-- Pledge results
+Select plg.pledge_donor_id, plg.pledge_pledge_number, 'P' As gypm_ind, plg.pledge_pledge_type,
+  tms_trans.short_desc, plg.pledge_date_of_record,
+  Cast (ksm_pkg.get_fiscal_year(plg.pledge_date_of_record) As varchar(4)) As fiscal_year,
+  -- Choose either amount paid or total amount
+  Case
+    When pp.prim_pledge_status != 'A' Then pp.prim_pledge_amount_paid
+    Else pp.prim_pledge_amount
+  End As pledge_amt,
+  pp.prim_pledge_status, plg.pledge_allocation_name, plg.pledge_program_code, plg.pledge_campaign
+From pledge plg
+  Inner Join primary_pledge pp On plg.pledge_pledge_number = pp.prim_pledge_number
+  Left Join tms_trans On plg.pledge_pledge_type = tms_trans.transaction_type_code
+Where plg.pledge_pledge_type Not In ('BE', 'LE')
+  And plg.pledge_program_code = 'KM'
+  And (ksm_pkg.get_fiscal_year(plg.pledge_date_of_record) >= 2008 Or plg.pledge_campaign = 'CCR')
+  And plg.pledge_associated_code = 'P'
+  And plg.pledge_amount > 0 -- Legal donors only
 )
+*/
