bd917cdbc6caab37a6324421a187c39c9f5dd7f5
Paul Hively <pbhively@gmail.com>
2017-03-21 15:17:31 -0500

    Campaign NGC rewrite; use existing daily table

--- sql code/ksm-campaign-ngc-transactions.sql
+++ sql code/ksm-campaign-ngc-transactions.sql
@@ -1,71 +1,43 @@
-/* See rpt_btaylor.nu_gft_t_ksm_dtl_curr for current counting method.
-   That table is updated by rpt_btaylor.wt099031_extracttransactions.
-
-Look into: receipt '0002275766' */
-
-With
-
--- Transaction types table
-tms_trans As (
+With 
+/* KSM-specific campaign new gifts & commitments definition */
+ksm_data As (
   (
-    Select transaction_type_code, short_desc
-    From tms_transaction_type
+  -- Kellogg campaign new gifts & commitments
+  Select id_number, record_type_code, person_or_org, birth_dt, category_code, rcpt_or_plg_number, xsequence,
+    credited_amount, amount, prim_amount,
+    year_of_giving, date_of_record, alloc_code, alloc_school, alloc_purpose, annual_sw, restrict_code,
+    transaction_type, pledge_status, gift_pledge_or_match, matched_donor_id, matched_receipt_number,
+    this_date, first_processed_date, std_area, zipcountry
+  From nu_rpt_t_cmmt_dtl_daily daily
+  Where daily.alloc_school = 'KM'
   ) Union All (
-    Select pledge_type_code, short_desc
-    From tms_pledge_type
-  ) Union All (
-    Select ' ' As matching_gift_code, 'Matching Gift' As short_desc
-    From dual
+  -- Internal transfer; 344303 is 50%
+  Select id_number, record_type_code, person_or_org, birth_dt, category_code, rcpt_or_plg_number, xsequence,
+    344303 As credited_amount, 344303 As amount, 344303 As prim_amount,
+    year_of_giving, date_of_record, alloc_code, alloc_school, alloc_purpose, annual_sw, restrict_code,
+    transaction_type, pledge_status, gift_pledge_or_match, matched_donor_id, matched_receipt_number,
+    this_date, first_processed_date, std_area, zipcountry
+  From nu_rpt_t_cmmt_dtl_daily daily
+  Where daily.rcpt_or_plg_number = '0002275766'
   )
+),
+/* Additional KSM-specific derived fields */
+ksm_campaign As (
+  Select ksm_data.*,
+    -- Giving bin
+    Case
+      When amount >= 1000000 Then 'A $1M+'
+      When amount >= 100000  Then 'B $100K-$999.9K'
+      When amount >= 50000   Then 'C $50K-$99.9K'
+      When amount >= 2500    Then 'D $2.5K-$49.9K'
+      When amount <  2500    Then 'E <$2.5K'
+    End As giving_band,
+    -- Replace null ksm_source_donor with id_number
+    NVL(ksm_pkg.get_gift_source_donor_ksm(rcpt_or_plg_number), id_number) As ksm_source_donor
+  From ksm_data
 )
 
-(
--- Gift and matching gift results from nu_gft_trp_gifttrans
-Select gft.id_number, gft.tx_number, gft.tx_gypm_ind, gft.transaction_type, tms_trans.short_desc,
-  gft.date_of_record, gft.fiscal_year, gft.legal_amount, gft.pledge_status, gft.allocation_code,
-  gft.alloc_short_name, gft.campaign_code
-From nu_gft_trp_gifttrans gft
-  Left Join tms_trans On tms_trans.transaction_type_code = gft.transaction_type
-Where gft.alloc_school = 'KM'
-  And (gft.fiscal_year >= 2008 Or gft.campaign_code = 'CCR')
-  And gft.legal_amount > 0 -- Legal donors only
-  And gft.tx_gypm_ind In ('G', 'M')
-
-)/* Union All (
--- Bequest and LE results
-Select plg.pledge_donor_id, plg.pledge_pledge_number, 'P' As gypm_ind, plg.pledge_pledge_type,
-  tms_trans.short_desc, plg.pledge_date_of_record,
-  Cast (ksm_pkg.get_fiscal_year(plg.pledge_date_of_record) As varchar(4)) As fiscal_year,
-  pp.discounted_amt, pp.prim_pledge_status, plg.pledge_allocation_name, plg.pledge_program_code,
-  plg.pledge_campaign
-From pledge plg
-  Inner Join primary_pledge pp On plg.pledge_pledge_number = pp.prim_pledge_number
-  Left Join tms_trans On plg.pledge_pledge_type = tms_trans.transaction_type_code
-Where plg.pledge_pledge_type In ('BE', 'LE')
-  And plg.pledge_program_code = 'KM'
-  And (ksm_pkg.get_fiscal_year(plg.pledge_date_of_record) >= 2008 Or plg.pledge_campaign = 'CCR')
-  And plg.pledge_associated_code = 'P'
-  And plg.pledge_amount > 0 -- Legal donors only
-  And pp.discounted_amt > 0
-
-) Union All (
--- Pledge results
-Select plg.pledge_donor_id, plg.pledge_pledge_number, 'P' As gypm_ind, plg.pledge_pledge_type,
-  tms_trans.short_desc, plg.pledge_date_of_record,
-  Cast (ksm_pkg.get_fiscal_year(plg.pledge_date_of_record) As varchar(4)) As fiscal_year,
-  -- Choose either amount paid or total amount
-  Case
-    When pp.prim_pledge_status != 'A' Then pp.prim_pledge_amount_paid
-    Else pp.prim_pledge_amount
-  End As pledge_amt,
-  pp.prim_pledge_status, plg.pledge_allocation_name, plg.pledge_program_code, plg.pledge_campaign
-From pledge plg
-  Inner Join primary_pledge pp On plg.pledge_pledge_number = pp.prim_pledge_number
-  Left Join tms_trans On plg.pledge_pledge_type = tms_trans.transaction_type_code
-Where plg.pledge_pledge_type Not In ('BE', 'LE')
-  And plg.pledge_program_code = 'KM'
-  And (ksm_pkg.get_fiscal_year(plg.pledge_date_of_record) >= 2008 Or plg.pledge_campaign = 'CCR')
-  And plg.pledge_associated_code = 'P'
-  And plg.pledge_amount > 0 -- Legal donors only
-)
-*/
+/* Main query */
+Select ksm_campaign.*, hh.household_id, hh.household_name, hh.household_spouse, hh.household_ksm_year, hh.household_program_group
+From ksm_campaign
+Inner Join table(ksm_pkg.tbl_entity_households_ksm) hh On ksm_campaign.ksm_source_donor = hh.id_number
