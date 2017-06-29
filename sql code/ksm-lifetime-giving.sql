0ac852a42bf9f4a5806bac8f220658fffaaf35fa
Paul Hively <pbhively@gmail.com>
2017-06-29 17:17:24 -0500

    Lifetime giving -- debugging pledges and matching gifts

--- sql code/ksm-lifetime-giving.sql
+++ sql code/ksm-lifetime-giving.sql
@@ -1,29 +1,32 @@
 With
 
-/* KSM allocations */
-ksm_alloc As (
-  Select allocation_code
-  From allocation
-  Where alloc_school = 'KM'
-),
-
 /* Primary pledge discounted amounts */
 plg_discount As (
-  Select pplg.prim_pledge_number As pledge_number, pplg.prim_pledge_type, pplg.prim_pledge_status,
-    pplg.prim_pledge_amount, pplg.prim_pledge_amount_paid, pplg.prim_pledge_original_amount, pplg.discounted_amt,
+  Select pledge.pledge_pledge_number As pledge_number, pledge.pledge_sequence, pplg.prim_pledge_type, pplg.prim_pledge_status,
+    pledge.pledge_amount, pplg.prim_pledge_amount, pplg.prim_pledge_amount_paid, pplg.prim_pledge_original_amount, pplg.discounted_amt,
     -- Discounted pledge credit amounts
     Case
       -- Not inactive, not a BE or LE
-      When pplg.prim_pledge_status Not In ('I', 'R') And pplg.prim_pledge_type Not In ('BE', 'LE') Then pplg.prim_pledge_amount
+      When (pplg.prim_pledge_status Is Null Or pplg.prim_pledge_status Not In ('I', 'R'))
+        And pplg.prim_pledge_type Not In ('BE', 'LE') Then pledge.pledge_associated_credit_amt
       -- Not inactive, is BE or LE
-      When pplg.prim_pledge_status Not In ('I', 'R') And pplg.prim_pledge_type In ('BE', 'LE') Then pplg.discounted_amt
+      When (pplg.prim_pledge_status Is Null Or pplg.prim_pledge_status Not In ('I', 'R'))
+        And pplg.prim_pledge_type In ('BE', 'LE') Then pplg.discounted_amt
       -- If inactive, take % of amount paid
-      Else pplg.prim_pledge_amount * (pplg.prim_pledge_amount_paid / pplg.prim_pledge_original_amount)
+      Else Case
+        When pplg.prim_pledge_original_amount > 0
+          Then pledge.pledge_associated_credit_amt * (pplg.prim_pledge_amount_paid / pplg.prim_pledge_original_amount)
+        Else 0
+      End
     End As credit
   From primary_pledge pplg
   Inner Join pledge On pledge.pledge_pledge_number = pplg.prim_pledge_number
-  Inner Join ksm_alloc On ksm_alloc.allocation_code = pledge.pledge_allocation_name
-  Where pledge.pledge_allocation_name In ksm_alloc.allocation_code
+  Where pledge.pledge_program_code = 'KM'
+    Or (
+      -- No program code, but Kellogg allocation school
+      pledge_program_code = ' '
+      And pledge_alloc_school = 'KM'
+    )
 ),
 
 /* Transaction and pledge TMS table definition */
@@ -39,22 +42,30 @@ tms_trans As (
 
 ksm_trans As (
   (
-    -- Outright gift and matches
-    Select id_number, tx_number, tms_trans.transaction_type, date_of_record, credit_amount
+    -- Outright gift
+    Select id_number, tx_number, tx_sequence, tms_trans.transaction_type, date_of_record, credit_amount
     From nu_gft_trp_gifttrans gft
     Left Join tms_trans On tms_trans.transaction_type_code = gft.transaction_type
     Where alloc_school = 'KM'
-      And tx_gypm_ind Not In ('Y', 'P') -- No pledges or pledge payments
+      And tx_gypm_ind = 'G'
   ) Union All (
-    -- Pledges and pledge payments, including BE and LE program credit
-    Select pledge_donor_id, pledge_pledge_number, tms_trans.transaction_type, pledge_date_of_record, credit
+    -- Matching gift
+    Select match_gift_matched_donor_id, match_gift_receipt_number, match_gift_matched_sequence, 'Matching Gift', match_gift_date_of_record, match_gift_amount
+    From matching_gift mg
+    Where match_gift_program_credit_code = 'KM'
+  ) Union All (
+    -- Pledges, including BE and LE program credit
+    Select pledge_donor_id, pledge_pledge_number, pledge.pledge_sequence, tms_trans.transaction_type, pledge_date_of_record, plgd.credit
     From pledge
     Inner Join tms_trans On tms_trans.transaction_type_code = pledge.pledge_pledge_type
-    Left Join plg_discount plgd On plgd.pledge_number = pledge.pledge_pledge_number
-    Left Join ksm_alloc On ksm_alloc.allocation_code = pledge.pledge_allocation_name
+    Left Join plg_discount plgd On plgd.pledge_number = pledge.pledge_pledge_number And plgd.pledge_sequence = pledge.pledge_sequence
     Where (
-      -- KSM allocations
-      pledge_allocation_name In ksm_alloc.allocation_code
+      -- KSM pledge credit
+      pledge_program_code = 'KM'
+    ) Or (
+      -- No program code, but Kellogg allocation school
+      pledge_program_code = ' '
+      And pledge_alloc_school = 'KM'
     ) Or (
       -- KSM program code
       pledge_allocation_name In ('BE', 'LE') -- BE and LE discounted amounts
@@ -63,8 +74,21 @@ ksm_trans As (
   )
 )
 
+/*
 Select
-  id_number,
-  sum(credit_amount) As credit_amount
+  entity.report_name,
+  ksm_trans.*
 From ksm_trans
-Group By id_number
+Inner Join entity On entity.id_number = ksm_trans.id_number
+Where entity.id_number = '0000598677'
+
+*/
+--/*
+Select
+  ksm_trans.id_number,
+  entity.report_name,
+  sum(ksm_trans.credit_amount) As credit_amount
+From ksm_trans
+Inner Join entity On entity.id_number = ksm_trans.id_number
+Group By ksm_trans.id_number, entity.report_name
+--*/
