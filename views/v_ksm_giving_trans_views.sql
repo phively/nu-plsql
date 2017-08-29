989f0e14028a265c65794a375e9b13491285cb3b
Paul Hively <pbhively@gmail.com>
2017-08-29 17:18:12 -0500

    KSM giving: HH campaign

--- views/v_ksm_giving_trans_views.sql
+++ views/v_ksm_giving_trans_views.sql
@@ -47,33 +47,27 @@ From table(ksm_pkg.tbl_gift_credit_campaign)
 
 Create or Replace View v_ksm_giving_campaign As
 With
--- View implementing householded campaign giving
+-- View implementing householded campaign giving based on new gifts & commitments
 hhid As (
   Select id_number, household_id, household_rpt_name, household_spouse_id, household_spouse
   From table(ksm_pkg.tbl_entity_households_ksm)
 ),
 cgft As (
   Select hhid.*,
-  Case When gft.id_number = household_id Then gft.credited_amount Else 0 End As hh_credit
+  Case When gft.id_number = household_id Then gft.credited_amount Else 0 End As hh_credit,
+  gft.year_of_giving fiscal_year
   From hhid
   Inner Join v_ksm_giving_campaign_trans gft On gft.id_number = hhid.id_number
 )
 Select Distinct hhid.id_number, entity.report_name, hhid.household_id, hhid.household_rpt_name, hhid.household_spouse_id, hhid.household_spouse,
-  sum(cgft.hh_credit) As campaign_giving
+  sum(cgft.hh_credit) As campaign_giving,
+  sum(Case When cal.curr_fy = fiscal_year     Then hh_credit Else 0 End) As campaign_cfy,
+  sum(Case When cal.curr_fy = fiscal_year + 1 Then hh_credit Else 0 End) As campaign_pfy1,
+  sum(Case When cal.curr_fy = fiscal_year + 2 Then hh_credit Else 0 End) As campaign_pfy2,
+  sum(Case When cal.curr_fy = fiscal_year + 3 Then hh_credit Else 0 End) As campaign_pfy3
 From hhid
+Cross Join v_current_calendar cal
 Inner Join cgft On hhid.household_id = cgft.household_id
 Inner Join entity On entity.id_number = hhid.id_number
 Group By hhid.id_number, entity.report_name, hhid.household_id, hhid.household_rpt_name, hhid.household_spouse_id, hhid.household_spouse
-
-/*
-  With
-  hhid As (
-    Select id_number, household_id
-    From table(ksm_pkg.tbl_entity_households_ksm)
-  )
-  Select hhid.household_id, ksm_trans.*,
-    Case When ksm_trans.id_number = household_id Then credit_amount Else 0 End As hh_credit
-  From table(tbl_gift_credit_ksm) ksm_trans
-  Inner Join hhid On hhid.id_number = ksm_trans.id_number;
-  */
 /
