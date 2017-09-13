81a17c539ccf7e9e71cd078f0417ed84f14ec5bf
Paul Hively <pbhively@gmail.com>
2017-09-13 11:47:14 -0500

    IR: name logic refactoring

--- sql code/IR FY17.sql
+++ sql code/IR FY17.sql
@@ -7,6 +7,10 @@ degs As (
 ),
 
 /* Household data */
+hhs As (
+  Select *
+  From table(ksm_pkg.tbl_entity_households_ksm)
+),
 hh As (
   Select hhs.*,
     entity.gender_code As gender, entity_s.gender_code As gender_spouse,
@@ -25,7 +29,7 @@ hh As (
       || (Case When entity_s.record_status_code = 'D' Then '<DECEASED>' End)
     ) As primary_name_spouse,
     degs.yrs, degs_s.yrs As yrs_spouse
-  From table(ksm_pkg.tbl_entity_households_ksm) hhs
+  From hhs hhs
   -- Names and strings for formatting
   Inner Join entity On entity.id_number = hhs.household_id
   Left Join entity entity_s On entity_s.id_number = hhs.household_spouse_id
@@ -91,15 +95,15 @@ fy_klc As (
 
 /* Loyal households */
 loyal_giving As (
-  Select Distinct hh.household_id,
+  Select Distinct hhs.household_id,
     -- WARNING: includes new gifts and commitments as well as cash
     sum(Case When fiscal_year = 2017 Then hh_credit Else 0 End) As stewardship_cfy,
     sum(Case When fiscal_year = 2016 Then hh_credit Else 0 End) As stewardship_pfy1,
     sum(Case When fiscal_year = 2015 Then hh_credit Else 0 End) As stewardship_pfy2
-  From table(ksm_pkg.tbl_entity_households_ksm) hh
+  From hhs
   Cross Join v_current_calendar cal
-  Inner Join v_ksm_giving_trans_hh gfts On gfts.household_id = hh.household_id
-  Group By hh.household_id
+  Inner Join v_ksm_giving_trans_hh gfts On gfts.household_id = hhs.household_id
+  Group By hhs.household_id
 ),
 loyal As (
   Select loyal_giving.*,
@@ -149,7 +153,7 @@ cgft As (
 
 /* Cash giving amounts */
 cash As (
-  Select Distinct hh.id_number, hh.household_id, hh.household_rpt_name, hh.household_spouse_id, hh.household_spouse,
+  Select Distinct hhs.id_number, hhs.household_id, hhs.household_rpt_name, hhs.household_spouse_id, hhs.household_spouse,
     -- Cash giving for KLC young alumni determination
     sum(Case When fiscal_year = 2012 And tx_gypm_ind <> 'P' Then hh_credit Else 0 End) As cash_fy12,
     sum(Case When fiscal_year = 2013 And tx_gypm_ind <> 'P' Then hh_credit Else 0 End) As cash_fy13,
@@ -157,25 +161,25 @@ cash As (
     sum(Case When fiscal_year = 2015 And tx_gypm_ind <> 'P' Then hh_credit Else 0 End) As cash_fy15,
     sum(Case When fiscal_year = 2016 And tx_gypm_ind <> 'P' Then hh_credit Else 0 End) As cash_fy16,
     sum(Case When fiscal_year = 2017 And tx_gypm_ind <> 'P' Then hh_credit Else 0 End) As cash_fy17
-  From table(ksm_pkg.tbl_entity_households_ksm) hh
-  Inner Join v_ksm_giving_trans_hh gfts On gfts.household_id = hh.household_id
-  Group By hh.id_number, hh.household_id, hh.household_rpt_name, hh.household_spouse_id, hh.household_spouse
+  From hhs
+  Inner Join v_ksm_giving_trans_hh gfts On gfts.household_id = hhs.household_id
+  Group By hhs.id_number, hhs.household_id, hhs.household_rpt_name, hhs.household_spouse_id, hhs.household_spouse
 ),
 
 /* Combine all criteria */
 donorlist As (
   (
   -- $2500+ cumulative campaign giving
-  Select cgft.*, hh.record_status_code, hh.household_suffix, hh.household_spouse_suffix, hh.household_masters_year,
-    hh.primary_name, hh.gender, hh.primary_name_spouse, hh.gender_spouse,
+  Select cgft.*, hh.record_status_code, hh.household_spouse_rpt_name, hh.household_suffix, hh.household_spouse_suffix,
+    hh.household_masters_year, hh.primary_name, hh.gender, hh.primary_name_spouse, hh.gender_spouse,
     hh.person_or_org, hh.yrs, hh.yrs_spouse
   From cgft
   Inner Join hh On hh.id_number = cgft.id_number
   Where cgft.campaign_giving >= 2500
   ) Union All (
   -- Young alumni giving $1000+ from FY12 on
-  Select cgft.*, hh.record_status_code, hh.household_suffix, hh.household_spouse_suffix, hh.household_masters_year,
-    hh.primary_name, hh.gender, hh.primary_name_spouse, hh.gender_spouse,
+  Select cgft.*, hh.record_status_code, hh.household_spouse_rpt_name, hh.household_suffix, hh.household_spouse_suffix,
+    hh.household_masters_year, hh.primary_name, hh.gender, hh.primary_name_spouse, hh.gender_spouse,
     hh.person_or_org, hh.yrs, hh.yrs_spouse
   From cgft
   Inner Join hh On hh.id_number = cgft.id_number
@@ -197,49 +201,84 @@ donorlist As (
           And (campaign_fy17 >= 1000 Or cash_fy17 >= 1000))
     )
   )
+),
+
+/* Name ordering helper */
+rec_name_logic As (
+  Select id_number, donorlist.household_id, primary_name, primary_name_spouse, household_rpt_name, household_spouse_rpt_name,
+    -- Name ordering based on rules we had discussed: alum first, if both or neither are alums then female first
+    Case
+      -- Anonymous donors take precedence
+      When anon.anon Is Not Null Or lower(primary_name) Like '%anonymous%donor%'
+        Then 'Anon'
+      -- Organizations next
+      When person_or_org = 'O' Then 'Org'
+      -- If no spouse, use own name
+      When primary_name_spouse Is Null Then 'Self'
+      -- If spouse, check if either/both have degrees
+      When primary_name_spouse Is Not Null Then
+        Case
+          -- If primary is only one with degrees, order is primary spouse
+          When yrs Is Not Null And yrs_spouse Is Null then 'Self Spouse'
+          -- If spouse is only one with degrees, order is spouse primary
+          When yrs Is Null And yrs_spouse Is Not Null Then 'Spouse Self'
+          -- Check gender
+          Else Case
+            -- If primary is female list primary first
+            When gender = 'F' Then 'Self Spouse'
+            -- If spouse is female list spouse first
+            When gender_spouse = 'F' Then 'Spouse Self'
+            -- Fallback
+            Else 'Self Spouse'
+          End
+        End
+    End As name_order  
+  From donorlist
+  Left Join anon On anon.household_id = donorlist.household_id
+),
+rec_name As (
+  Select rn.id_number, anon.anon,
+    -- Proposed recognition name
+    (Case
+      -- Fully anonymous donors are just Anonymous
+      When rn.name_order = 'Anon' Then 'Anonymous'
+      -- Orgs get their full name
+      When rn.name_order = 'Org' Then household_rpt_name
+      When rn.name_order = 'Self' Then trim(primary_name)
+      When rn.name_order = 'Self Spouse' Then trim(primary_name || ' and ' || primary_name_spouse)
+      When rn.name_order = 'Spouse Self' Then trim(primary_name_spouse || ' and ' || primary_name)
+    End
+      -- Add loyal tag if applicable
+      || (Case When rn.name_order <> 'Anon' Then loyal.loyal End)
+      -- Add KLC tag if applicable
+      || (Case When rn.name_order Not In('Anon', 'Org') Then fy_klc.klc End)
+    ) As proposed_recognition_name,
+    -- Proposed sort name within groups
+    Case
+      When rn.name_order = 'Anon' Then '00Anonymous'
+      When rn.name_order = 'Org' Then household_rpt_name
+      When rn.name_order = 'Self' Then household_rpt_name
+      When rn.name_order = 'Self Spouse' Then household_rpt_name || '; ' || household_spouse_rpt_name
+      When rn.name_order = 'Spouse Self' Then household_spouse_rpt_name || '; ' || household_rpt_name
+    End As proposed_sort_name
+  From rec_name_logic rn
+  Left Join fy_klc On fy_klc.household_id = rn.household_id
+  Left Join loyal On loyal.household_id = rn.household_id
+  Left Join anon On anon.household_id = rn.household_id
 )
 
 /* Main query */
 Select Distinct
   -- Print in IR flag, for household deduping
-  'Y' As print_in_report
-  -- Recognition name string; fully anonymous donors are just Anonymous
-  Case When anon.anon Is Not Null Then 'Anonymous'
-  Else
-    -- All others
-    (Case
-      -- Orgs get their full name
-      When person_or_org = 'O' Then household_rpt_name
-      -- If no spouse, use own name
-      When primary_name_spouse Is Null Then trim(primary_name)
-      -- If spouse, check if either/both have degrees
-      When primary_name_spouse Is Not Null Then
-        Case
-          -- If primary is only one with degrees, order is primary spouse
-          When yrs Is Not Null And yrs_spouse Is Null Then primary_name || ' and ' || primary_name_spouse
-          -- If spouse is only one with degrees, order is spouse primary
-          When yrs Is Null And yrs_spouse Is Not Null Then primary_name_spouse || ' and ' || primary_name
-          -- Check gender
-          Else Case
-            -- If primary is female list primary first
-            When gender = 'F' Then primary_name || ' and ' || primary_name_spouse
-            -- If spouse is female list spouse first
-            When gender_spouse = 'F' Then primary_name_spouse || ' and ' || primary_name
-            -- Fallback
-            Else primary_name || ' and ' || primary_name_spouse
-          End
-        End
-    End
-    -- Add loyal tag if applicable
-    || loyal.loyal
-    -- Add KLC tag if applicable
-    || fy_klc.klc)
-  End As proposed_recognition_name,
+--  'Y' As print_in_report
+  -- Recognition name string
+  rec_name.proposed_sort_name,
+  rec_name.proposed_recognition_name,
   -- Giving level string
   proposed_giving_level,
   -- Anonymous flags
-  Case When proposed_giving_level <> nonanon_giving_level And anon.anon Is Null Then nonanon_giving_level End As different_nonanon_level,
-  anon.anon,
+  Case When proposed_giving_level <> nonanon_giving_level And rec_name.anon Is Null Then nonanon_giving_level End As different_nonanon_level,
+  rec_name.anon,
   anon_thru_fy17,
   nonanon_thru_fy17,
   -- Fields
@@ -259,7 +298,7 @@ Select Distinct
   gender,
   household_masters_year,
   household_spouse_id,
-  household_spouse,
+  household_spouse_rpt_name,
   household_spouse_suffix,
   primary_name_spouse,
   yrs_spouse,
@@ -283,9 +322,8 @@ Select Distinct
   campaign_fy17,
   campaign_fy18
 From donorlist
+Inner Join rec_name On rec_name.id_number = donorlist.id_number
 Left Join assign_conc On assign_conc.household_id = donorlist.household_id
-Left Join dec_spouse_conc On dec_spouse_conc.id_number = donorlist.id_number
-Left Join fy_klc On fy_klc.household_id = donorlist.household_id
 Left Join loyal On loyal.household_id = donorlist.household_id
-Left Join anon On anon.household_id = donorlist.household_id
-Order By proposed_giving_level Asc, household_rpt_name Asc
+Left Join dec_spouse_conc On dec_spouse_conc.id_number = donorlist.id_number
+Order By proposed_giving_level Asc, rec_name.proposed_sort_name Asc
