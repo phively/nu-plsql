7016a9435233854bd9ff180783427777fa8c725c
Paul Hively <pbhively@gmail.com>
2018-02-09 16:20:01 -0600

    Delete old AF Tableau views

--- views/tableau_af_views.sql
+++ views/tableau_af_views.sql
@@ -1,3 +1,7 @@
+/*****************************************
+Core Annual Giving view - transactions by source donor
+******************************************/
+
 Create Or Replace View vt_af_gifts_srcdnr_5fy As
 With
 
@@ -647,4 +651,48 @@ Left Join allocation allocm On allocm.allocation_code = matching_gift.match_gift
 Where match_gift_program_credit_code <> 'KM'
   And gft.alloc_school = 'KM'
 Order By match_gift_date_of_record Desc
+;
+
+/*****************************************
+Gifts made to TBD accounts
+******************************************/
+
+Create Or Replace View vt_alloc_tbd_accounts As
+
+With
+
+/* All Kellogg TBD accounts */
+ksm_tbd As (
+  Select *
+  From allocation alloc
+  Where lower(alloc.short_name) Like '%kellogg%tbd%'
+),
+
+/* KSM degrees concat definition */
+deg As (
+  Select *
+  From table(rpt_pbh634.ksm_pkg.tbl_entity_degrees_concat_ksm)
+),
+
+/* Transaction and pledge TMS table definition */
+tms_trans As (
+  (
+    Select transaction_type_code, short_desc As transaction_type
+    From tms_transaction_type
+  ) Union All (
+    Select pledge_type_code, short_desc
+    From tms_pledge_type
+  )
+)
+
+/* Main query */
+Select
+  gft.id_number, donor_name, record_type_code, deg.degrees_concat, trim(deg.program_group) As program_group,
+  tx_number, tx_sequence, date_of_record, fiscal_year, legal_amount, credit_amount, gft.tx_gypm_ind, tms_trans.transaction_type,
+  gft.allocation_code, gft.alloc_short_name
+From nu_gft_trp_gifttrans gft
+Inner Join ksm_tbd On gft.allocation_code = ksm_tbd.allocation_code
+Left Join deg On deg.id_number = gft.id_number
+Left Join tms_trans On tms_trans.transaction_type_code = gft.transaction_type
+Order By date_of_record Desc, credit_amount Desc, tx_sequence Asc
 ;
