c5cf994142321f5f14c41b9f08d1781a9b88551a
Paul Hively <pbhively@gmail.com>
2018-02-27 14:26:10 -0600

    Geocoding fields update

--- sql code/ksm-prospects-geocoding.sql
+++ sql code/ksm-prospects-geocoding.sql
@@ -21,12 +21,16 @@ Select
   , address.street1
   , address.street2
   , address.street3
+  , trim(address.street1 || ' ' || address.street2 || ' ' || address.street3)
+    As address_street
   , address.city
   , address.state_code
-  , address.foreign_cityzip
   , address.zipcode
+  , address.foreign_cityzip
   , address.zip_suffix
   , tmsc.short_desc As country
+  , Case When address.country_code Is Null Or address.country_code In ('US', '', ' ') Then 'Y' End
+    As usa_ind
 From addrs
 Inner Join address On address.id_number = addrs.id_number
   And address.xsequence = addrs.xsequence
