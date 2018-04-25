007ebe6c38b05cf283e56eb5ab9eab4d63bd1467
Paul Hively <pbhively@gmail.com>
2018-04-25 14:20:29 -0500

    Portfolio view code cleanup, commenting

--- views/tableau_portfolio_metrics.sql
+++ views/tableau_portfolio_metrics.sql
@@ -2,48 +2,33 @@
 
 With
 
--- '0000549376'
-
 -- Assignment history
 assignments As (
   Select
-    prospect_id
-    , id_number
-    , report_name
-    , assignment_type
-    , assignment_type_desc
-    , start_dt_calc
-    , stop_dt_calc
-    , Case When stop_dt_calc Is Null Then last_day(cal.today) Else stop_dt_calc End
-      As stop_dt_calc_filled
-    , assignment_active_calc
-    , assignment_id_number
-    , assignment_report_name
-  From v_assignment_history
-  Cross Join rpt_pbh634.v_current_calendar cal
-  Where assignment_type In ('PM', 'PP') -- PM and PPM only
-    And primary_ind = 'Y'
-/****** FOR TESTING -- REMOVE LATER ******/
-And assignment_id_number = '0000549376'
-)
-
--- 
-, assn_numbered As (
-  Select
-    rownum As rn
+    rownum As rn -- Number each row; important for assn_dense
     , prospect_id
     , id_number
     , report_name
     , assignment_type
     , assignment_type_desc
     , start_dt_calc As start_dt
-    , stop_dt_calc_filled As stop_dt
-    , ceil(months_between(last_day(stop_dt_calc_filled), trunc(start_dt_calc, 'month')))
-      As nmonths
+    -- Fill in the end of this month if there is no stop_dt_calc
+    , Case When stop_dt_calc Is Null Then last_day(cal.today) Else stop_dt_calc End
+      As stop_dt
+    -- Number of months from start_dt_calc to stop_dt_calc, rounded up
+    , ceil(
+        months_between(last_day(Case When stop_dt_calc Is Null Then last_day(cal.today) Else stop_dt_calc End)
+        , trunc(start_dt_calc, 'month'))
+      ) As nmonths
     , assignment_active_calc
     , assignment_id_number
     , assignment_report_name
-  From assignments
+  From v_assignment_history
+  Cross Join rpt_pbh634.v_current_calendar cal
+  Where assignment_type In ('PM', 'PP') -- PM and PPM only
+    And primary_ind = 'Y' -- Primary prospect entity only
+/****** FOR TESTING -- REMOVE LATER ******/
+And assignment_id_number = '0000549376'
 )
 
 -- Assignment history by month between start_dt and stop_dt
@@ -56,19 +41,23 @@ And assignment_id_number = '0000549376'
     , assignment_type_desc
     , start_dt
     , stop_dt
-    -- Take either 1 month in the future from last row, or the stop_dt_calc, whichever is smaller
+    -- Take either 1 month in the future from last row, or the stop_dt, whichever is smaller
     , least(
-        Case When level = 1 Then start_dt Else trunc(add_months(start_dt, level - 1), 'month') End
+        Case
+          When level = 1 Then start_dt -- First filled_date is start_dt
+          When level = nmonths Then stop_dt -- Last filled date is stop_dt
+          Else trunc(add_months(start_dt, level - 1), 'month') -- Subsequent are 1st of month after previous row
+        End
         , stop_dt
       ) As filled_date
     , assignment_active_calc
     , assignment_id_number
     , assignment_report_name
-  From assn_numbered
+  From assignments
   Connect By
-    level <= nmonths
-    And Prior rn = rn
-    And Prior dbms_random.value != 1 -- Always false; 0 < dbms_random.value < 1
+    level <= nmonths -- Hierarchical query
+    And Prior rn = rn -- Restart when prospect/manager changes, since each prospect/pm combo has its own row
+    And Prior dbms_random.value != 1 -- Always false, as 0 < dbms_random.value < 1
 )
 
 -- Stage history
