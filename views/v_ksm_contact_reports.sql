5e64f60fac475fc5c705f548f30984802a4ec99a
Paul Hively <pbhively@gmail.com>
2018-04-26 15:01:40 -0500

    Contact reports: new fast non-householded view

--- views/v_ksm_contact_reports.sql
+++ views/v_ksm_contact_reports.sql
@@ -1,8 +1,8 @@
 /****************************
-All ARD contact reports
+All NU contact reports; fast due to no householding
 ****************************/
 
-Create Or Replace View v_contact_reports As
+Create Or Replace View v_contact_reports_fast As
 
 With
 
@@ -12,16 +12,6 @@ pe As (
   From prospect_entity pre
   Inner Join prospect p On p.prospect_id = pre.prospect_id
   Where p.active_ind = 'Y'
-)
-
-/* KSM households -- only those with contact reports */
-, hh As (
-  Select Distinct
-    contact_report.id_number
-    , hhs.report_name
-    , hhs.household_id
-  From contact_report
-  Inner Join table(rpt_pbh634.ksm_pkg.tbl_entity_households_ksm) hhs On hhs.id_number = contact_report.id_number
 )
 
 /* ARD staff */
@@ -61,7 +51,6 @@ Select
   , contact_report.id_number
   , contact_report.contacted_name
   , contacted_entity.report_name
-  , contacted_entity.household_id
   , contact_report.prospect_id
   , pe.primary_ind
   , prospect.prospect_name
@@ -100,13 +89,73 @@ Inner Join contact_rpt_credit On contact_rpt_credit.report_id = contact_report.r
 Inner Join tms_contact_rpt_purpose tms_cpurp On tms_cpurp.contact_purpose_code = contact_report.contact_purpose_code
 Inner Join tms_contact_rpt_type tms_ctype On tms_ctype.contact_type = contact_report.contact_type
 Inner Join nu_prs_trp_prospect prs On prs.id_number = contact_report.id_number
-Inner Join hh contacted_entity On contacted_entity.id_number = contact_report.id_number
+Inner Join entity contacted_entity On contacted_entity.id_number = contact_report.id_number
 Inner Join entity contacter On contacter.id_number = contact_rpt_credit.id_number
 Left Join ard_staff On ard_staff.id_number = contact_rpt_credit.id_number
 Left Join table(ksm_pkg.tbl_frontline_ksm_staff) ksm_staff On ksm_staff.id_number = ard_staff.id_number
 Left Join prospect On prospect.prospect_id = prs.prospect_id
 Left Join pe On pe.id_number = prs.id_number
 Left Join table(ksm_pkg.tbl_university_strategy) strat On strat.prospect_id = contact_report.prospect_id
+;
+
+/****************************
+All NU contact reports with householding
+****************************/
+
+Create Or Replace View v_contact_reports As
+
+With
+
+/* KSM households -- only those with contact reports */
+hh As (
+  Select Distinct
+    contact_report.id_number
+    , hhs.report_name
+    , hhs.household_id
+  From contact_report
+  Inner Join table(rpt_pbh634.ksm_pkg.tbl_entity_households_ksm) hhs On hhs.id_number = contact_report.id_number
+)
+
+/* Main query */
+Select
+  credited
+  , credited_name
+  , job_title
+  , employer_unit
+  , contact_type
+  , contact_purpose
+  -- Contact report fields
+  , report_id
+  , vcrf.id_number
+  , contacted_name
+  , vcrf.report_name
+  , hh.household_id
+  , prospect_id
+  , primary_ind
+  , prospect_name
+  , prospect_name_sort
+  , contact_date
+  , fiscal_year
+  , description
+  , summary
+  -- Prospect fields
+  , officer_rating
+  , evaluation_rating
+  , university_strategy
+  -- Custom variables
+  , ard_staff
+  , frontline_ksm_staff
+  , contact_type_category
+  , visit_type
+  , rating_bin
+  , curr_fy
+  , prev_fy_start
+  , curr_fy_start
+  , next_fy_start
+  , yesterday
+  , ninety_days_ago
+From v_contact_reports_fast vcrf
+Inner Join hh On hh.id_number = vcrf.id_number
 ;
 
 /****************************
