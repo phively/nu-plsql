f49dff323b80e1716ecda1893169adfcb1f1305a
Paul Hively <pbhively@gmail.com>
2019-01-17 17:03:08 -0600

    NU visits
    - Created v_nu_visits to pull all visits since the previous fiscal year
    - v_ksm_visits now reference v_nu_visits

--- views/v_ksm_visits.sql
+++ views/v_ksm_visits.sql
@@ -1,15 +1,24 @@
-Create Or Replace View v_ksm_visits As
+/* Cleaned up visits view */
+Create Or Replace View v_nu_visits As
 
 With
 
 /*
 Visits from the beginning of the previous fiscal year to yesterday
-Must be made by Kellogg frontline staff
 Only inlcudes living entities, but prospect records may have since been deactivated
 */
 
+/* VIP visits */
+vips As (
+  Select '0000573302' As id_number, 'President' As affil, NULL As start_dt, NULL As stop_dt From DUAL -- MOS
+  Union 
+  Select '0000299349', 'Dean', to_date('20100701', 'yyyymmdd'), to_date('20180831', 'yyyymmdd') From DUAL -- SB
+  Union
+  Select '0000246649', 'Dean', to_date('20180901', 'yyyymmdd'), to_date('20500831', 'yyyymmdd') From DUAL -- KH
+)
+
 /* TMS values for purpose (e.g. qualification, solicitation, ...) */
-tms_cpurp As (
+, tms_cpurp As (
   Select
     contact_purpose_code
     , short_desc
@@ -34,7 +43,6 @@ Select
   , staff.report_name As credited_name
   , tms_crc.contact_credit_type
   , tms_crc.short_desc As contact_credit_desc
-  , staff.job_title
   , tms_cpurp.short_desc As contact_purpose
   -- Contact report fields
   , contact_report.report_id
@@ -56,16 +64,63 @@ Select
       When evaluation_rating <> ' ' Then eval.numeric_bin
       Else 0
     End As rating_bin
+  , Case
+      When vips.affil = 'President'
+        Then 'Y'
+      End
+    As president_visit
+  , Case
+      When vips.affil = 'Dean'
+        And contact_report.contact_date Between start_dt And stop_dt
+        Then 'Y'
+      End
+    As dean_visit
   , cal.curr_fy
 From contact_report
 Cross Join cal
 Inner Join contact_rpt_credit On contact_rpt_credit.report_id = contact_report.report_id
 Inner Join tms_cpurp On tms_cpurp.contact_purpose_code = contact_report.contact_purpose_code
-Inner Join table(ksm_pkg.tbl_frontline_ksm_staff) staff On staff.id_number = contact_rpt_credit.id_number
+Inner Join entity staff
+  On staff.id_number = contact_rpt_credit.id_number
 Left Join tms_contact_rpt_credit_type tms_crc On tms_crc.contact_credit_type = contact_rpt_credit.contact_credit_type
 Left Join nu_prs_trp_prospect prs On prs.id_number = contact_report.id_number
 Left Join table(ksm_pkg.tbl_university_strategy) strat On strat.prospect_id = contact_report.prospect_id
+Left Join vips On vips.id_number = contact_rpt_credit.id_number
 Left Join rating_bins eval On eval.rating_desc = prs.evaluation_rating
 Left Join rating_bins uor On uor.rating_desc = prs.officer_rating
 Where contact_report.contact_date Between cal.prev_fy_start And cal.yesterday
   And contact_report.contact_type = 'V'
+;
+
+/* Kellogg-specific visits */
+Create Or Replace View v_ksm_visits As
+
+/* Only the visits made by Kellogg frontline staff */
+
+Select
+  credited
+  , staff.report_name As credited_name
+  , contact_credit_type
+  , contact_credit_desc
+  , staff.job_title
+  , contact_purpose
+  -- Contact report fields
+  , report_id
+  , id_number
+  , contacted_name
+  , prospect_id
+  , contact_date
+  , fiscal_year
+  , description
+  , summary
+  -- Prospect fields
+  , officer_rating
+  , evaluation_rating
+  , university_strategy
+  -- Custom variables
+  , visit_type
+  , rating_bin
+  , curr_fy
+From v_nu_visits
+Inner Join table(ksm_pkg.tbl_frontline_ksm_staff) staff On staff.id_number = v_nu_visits.credited
+;
