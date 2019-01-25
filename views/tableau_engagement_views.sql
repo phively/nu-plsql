b242a3e64caff6b55065d2a6eb9a92c0e5c92148
Paul Hively <pbhively@gmail.com>
2019-01-25 15:47:51 -0600

    Engagement view update
    - Tweaked visit definitions
    - Started merged pull to "union" together the different engagement types

--- views/tableau_engagement_views.sql
+++ views/tableau_engagement_views.sql
@@ -1,13 +1,71 @@
--- Create Or Replace View vt_engagement_detail
+/* Current and previous fiscal year engagement details */
+-- Create Or Replace View vt_engagement_detail As
+
+With
+
+-- Current calendar
+cal As (
+  Select
+    yesterday
+    , curr_fy
+    , prev_fy_start
+    , curr_fy_start
+    , next_fy_start
+  From v_current_calendar cal
+)
 
 -- Visits
-Select Distinct
-  v.*
-  , Case When ah.assignment_report_name Is Not Null Then 'Y' End As pm_visit
-From v_nu_visits v
-Cross Join v_current_calendar cal
-Left Join v_assignment_history ah
-  On v.credited = ah.assignment_id_number
-  And v.prospect_id = ah.prospect_id
-  And assignment_type = 'PM'
-  And v.contact_date Between ah.start_dt_calc And nvl(ah.stop_dt_calc, cal.next_fy_start)
+, visits As (
+  Select Distinct
+    v.*
+    , Case When ah.assignment_report_name Is Not Null Then 'Y' End As pm_visit
+  From v_nu_visits v
+  Cross Join cal
+  Left Join v_assignment_history ah
+    On v.credited = ah.assignment_id_number
+    And v.prospect_id = ah.prospect_id
+    And assignment_type = 'PM'
+    And v.contact_date Between ah.start_dt_calc And nvl(ah.stop_dt_calc, cal.next_fy_start)
+  Where v.fiscal_year Between cal.curr_fy - 1 And cal.curr_fy
+)
+
+-- Merged pull
+-- Visits
+Select
+  visits.id_number
+  , 'Visit'
+    As engagement_type
+  , Case
+      When president_visit = 'Y'
+        Then 'President Visit'
+      When ksm_dean_visit = 'Y'
+        Then 'Dean Visit'
+      When pm_visit = 'Y'
+        Then 'PM Visit'
+      Else 'Other Visit'
+      End
+    As engagement_type_detail
+  , visit_type
+    As type_detail
+  , credited_name
+    As engagement_responsible
+  , description
+    As engagement_detail
+  , report_id
+    As engagement_id
+  , contact_date
+    As engagement_date
+  , fiscal_year
+    As engagement_fy
+From visits
+-- Committees
+-- Events
+-- Activities
+-- Giving
+
+-- Final pull
+-- id_number
+-- , report_name
+-- , degrees_concat
+-- , inst_suffix
+-- , PM
