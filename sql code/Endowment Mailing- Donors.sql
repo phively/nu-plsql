22c50dbac24cc5ea58d73af9c4b0016e06f8c1e8
Paul Hively <pbhively@gmail.com>
2019-04-08 17:02:51 -0500

    Endowment donors: formatting pass

--- sql code/Endowment Mailing- Donors.sql
+++ sql code/Endowment Mailing- Donors.sql
@@ -1,226 +1,269 @@
 With
 
-Rep_End_Alloc AS (
-       Select a.allocation_code
-       From allocation a
-       Where steward_reporting_code = 'RP'
-       AND alloc_school = 'KM'
-       AND Agency = 'END'
-),
+rep_end_alloc As (
+  Select a.allocation_code
+  From allocation a
+  Where steward_reporting_code = 'RP'
+    And alloc_school = 'KM'
+    And agency = 'END'
+)
 
-HH_END_Trans AS (
+, hh_end_trans As (
   Select gft.*
   From rpt_pbh634.v_ksm_giving_campaign_trans_hh gft
   Where
-  -- Reportable Endowed allocations per Rep_End_Alloc subquery
+  -- Reportable Endowed allocations per rep_end_alloc subquery
   gft.allocation_code In (Select Distinct allocation_code From Rep_End_Alloc)
-),
-
-HH_End_Donors AS (
-    SELECT
-      trans."HOUSEHOLD_ID"
-      ,SUM(trans."HH_RECOGNITION_CREDIT") AS TOTAL_TO_END
-    FROM HH_END_Trans trans
-    GROUP BY trans.HOUSEHOLD_ID
- ),
-              
-Spouse AS(
-   SELECT 
-  E.ID_NUMBER
-  ,E.SPOUSE_ID_NUMBER
-  ,SE.RECORD_STATUS_CODE
-  ,SE.PREF_MAIL_NAME
-  ,SE.FIRST_NAME
-  ,SE.LAST_NAME
-FROM ENTITY E
-LEFT JOIN ENTITY SE
-ON E.SPOUSE_ID_NUMBER = SE.ID_NUMBER
-WHERE
-SE.RECORD_STATUS_CODE = 'A'
-),
-
-Sally_Salut AS(
-Select
-      ID_Number
-    , max(salutation) keep(dense_rank First Order By ID_number Asc, date_modified) AS Latest_Sal
-    From salutation
-    WHERE signer_ID_number = '0000299349'
-    AND Active_IND = 'Y'
-    AND salutation != '%and%'
-    Group by ID_Number
-    Order by ID_number
-),
-
-PrefAddress AS( 
-Select 
-   a.Id_number
-,  tms_addr_status.short_desc AS Address_Status
-,  tms_address_type.short_desc AS Address_Type
-,  a.addr_pref_ind
-,  a.company_name_1
-,  a.company_name_2
-,  a.street1
-,  a.street2
-,  a.street3
-,  a.foreign_cityzip
-,  a.city
-,  a.state_code
-,  a.zipcode
-,  tms_country.short_desc AS Country
-FROM address a
-INNER JOIN tms_addr_status
-ON tms_addr_status.addr_status_code = a.addr_status_code
-LEFT JOIN tms_address_type
-ON tms_address_type.addr_type_code = a.addr_type_code
-LEFT JOIN tms_country
-ON tms_country.country_code = a.country_code
-WHERE a.addr_pref_IND = 'Y'
-AND a.addr_status_code IN('A','K')
-),
-
-Seas_Address AS(
-SELECT *
-FROM RPT_DGZ654.v_seasonal_addr SA
-WHERE CURRENT_DATE
-  BETWEEN SA.real_start_date1 AND SA.real_stop_date1 
-  OR CURRENT_DATE BETWEEN SA.real_start_date2 AND SA.real_stop_date2
-),
-
-Prospect_Manager AS (
-     Select A.prospect_ID, PET.ID_Number, A.assignment_ID_Number, E.Pref_Mail_Name
-     From assignment A
-     Inner Join prospect_entity PET
-     ON A.prospect_ID = PET.prospect_Id
-     Inner Join entity E
-     ON A.assignment_id_number = E.id_number
-     Where A.assignment_type = 'PM'
-     AND A.active_IND = 'Y'
-),
-
-Prog_Prospect_Manager AS (
-     Select 
-     A.prospect_ID
-     , PET.ID_Number
-     , listagg(A.assignment_ID_Number, ', ') within group (order by A.Assignment_Id_Number Asc) AS PPM_IDs
-     , listagg(E.Pref_Mail_Name, ', ') within group (order by A.Assignment_Id_Number Asc) AS PPM_Names
-     From assignment A
-     Inner Join prospect_entity PET
-     ON A.prospect_ID = PET.prospect_Id
-     Inner Join entity E
-     ON A.assignment_id_number = E.id_number
-     Where A.assignment_type = 'PP'
-     AND A.active_IND = 'Y'
-     AND PET.PRIMARY_IND = 'Y'
-     Group by A.Prospect_ID, PET.ID_Number
 )
 
-SELECT DISTINCT
-  E.ID_NUMBER AS "Household ID"
-  ,E.Report_name as "P Report Name"
-  ,E.first_name AS "P First Name"
-  ,PSAL.latest_sal AS "P Sally Salut"
-  ,E.Pref_Mail_Name
-  ,E.RECORD_STATUS_CODE "P Record Status" 
-  ,D.DEGREES_CONCAT P_Degrees_Concat
-  ,E.spouse_ID_Number AS "Spouse ID"
-  ,S.record_status_code AS "Spouse_Status"
-  ,S.pref_mail_name AS "Spouse_Pref_Name"
-  ,S.first_name  AS "Spouse_First_Name"
-  ,S.pref_mail_name AS "Spouse_Pref_Name"
-  ,SSAL.latest_sal  AS "Sally_Spouse_Salut"
-  ,E.JNT_SALUTATION AS "Joint Salutation"
-  ,PM.pref_Mail_Name AS "Prospect Manager"
-  ,PP.PPM_Names AS "Program Prospect Manager"
-  ,Don.Total_To_End
+, hh_end_donors As (
+  Select
+    trans.household_id
+    , sum(trans.hh_recognition_credit) As total_to_end
+  From hh_end_trans trans
+  Group By trans.household_id
+)
+
+, spouse As (
+  Select 
+    e.id_number
+    ,e.spouse_id_number
+    ,se.record_status_code
+    ,se.pref_mail_name
+    ,se.first_name
+    ,se.last_name
+  From entity e
+  Left Join entity se
+    On e.spouse_id_number = se.id_number
+  Where se.record_status_code = 'A'
+)
+
+, sally_salut As (
+  Select
+    id_number
+    , max(salutation) keep(dense_rank First Order By ID_number Asc, date_modified)
+      As latest_sal
+  From salutation
+  Where signer_id_number = '0000299349'
+    And active_ind = 'Y'
+    And salutation != '%and%'
+  Group By id_number
+  Order By id_number
+)
+
+, prefaddress As (
+  Select 
+    a.id_number
+    , tms_addr_status.short_desc
+      As address_status
+    , tms_address_type.short_desc
+      As address_type
+    , a.addr_pref_ind
+    , a.company_name_1
+    , a.company_name_2
+    , a.street1
+    , a.street2
+    , a.street3
+    , a.foreign_cityzip
+    , a.city
+    , a.state_code
+    , a.zipcode
+    , tms_country.short_desc
+      As country
+  From address a
+  Inner Join tms_addr_status
+    On tms_addr_status.addr_status_code = a.addr_status_code
+  Left Join tms_address_type
+    On tms_address_type.addr_type_code = a.addr_type_code
+  Left Join tms_country
+    On tms_country.country_code = a.country_code
+  Where a.addr_pref_ind = 'Y'
+  And a.addr_status_code In ('A','K')
+)
+
+, seas_address As (
+  Select *
+  From rpt_dgz654.v_seasonal_addr sa
+  Where current_date Between sa.real_start_date1 And sa.real_stop_date1 
+    Or current_date Between sa.real_start_date2 And sa.real_stop_date2
+)
+
+, prospect_manager As (
+  Select
+    a.prospect_id
+    , pet.id_number
+    , a.assignment_id_number
+    , e.pref_mail_name
+  From assignment a
+  Inner Join prospect_entity pet
+    On a.prospect_id = pet.prospect_id
+  Inner Join entity e
+   On a.assignment_id_number = e.id_number
+  Where a.assignment_type = 'PM'
+  And a.active_ind = 'Y'
+)
+
+, prog_prospect_manager As (
+  Select 
+    a.prospect_id
+    , pet.id_number
+    , listagg(a.assignment_id_number, ', ') Within Group (Order By a.assignment_id_number Asc)
+      As ppm_ids
+    , listagg(e.pref_mail_name, ', ') Within Group (Order By a.assignment_id_number Asc)
+      As ppm_names
+  From assignment a
+  Inner Join prospect_entity pet
+    On a.prospect_id = pet.prospect_id
+  Inner Join entity e
+  On a.assignment_id_number = e.id_number
+  Where a.assignment_type = 'PP'
+    And a.active_ind = 'Y'
+    And pet.primary_ind = 'Y'
+  Group By
+    a.prospect_id
+    , pet.id_number
+)
+
+Select Distinct
+  e.id_number As "Household ID"
+  , e.report_name As "P Report Name"
+  , e.first_name As "P First Name"
+  , psal.latest_sal As "P Sally Salut"
+  , e.pref_mail_name
+  , e.record_status_code As "P Record Status" 
+  , d.degrees_concat As p_degrees_concat
+  , e.spouse_id_number As "Spouse ID"
+  , s.record_status_code As "Spouse_Status"
+  , s.pref_mail_name As "Spouse_Pref_Name"
+  , s.first_name As "Spouse_First_Name"
+  , s.pref_mail_name As "Spouse_Pref_Name"
+  , ssal.latest_sal As "Sally_Spouse_Salut"
+  , e.jnt_salutation As "Joint Salutation"
+  , pm.pref_mail_name As "Prospect Manager"
+  , pp.ppm_names As "Program Prospect Manager"
+  , don.total_to_end
 --  ,Case
 --     When e.first_Name IS NOT Null AND s.spouse_first_name IS NOT Null
 --       Then E.first_name ||' and '||S.spouse_first_name
 --         Else 'Friends'
 --           End Joint_First_Name
-  ,Case
-     When SA.ADDRESS_TYPE = 'Seasonal' Then 'Seasonal'
-     When PA.Address_type IS NOT Null Then 'Preferred'
-     Else Null
-       End Addr_Type
-  ,Case
-     When SA.ADDRESS_TYPE = 'Seasonal' Then SA.Company_Name_1
-     When PA.Address_type IS NOT Null Then PA.Company_Name_1
-     Else Null
-       End Company_Name_1
-   ,Case
-     When SA.ADDRESS_TYPE = 'Seasonal' Then SA.Company_Name_2
-     When PA.Address_type IS NOT Null Then PA.Company_Name_2
-     Else Null
-       End Company_Name_2
-   ,Case
-     When SA.ADDRESS_TYPE = 'Seasonal' Then SA.Street1
-     When PA.Address_type IS NOT Null Then PA.Street1
-     Else Null
-       End Street1
-   ,Case
-     When SA.ADDRESS_TYPE = 'Seasonal' Then SA.Street2
-     When PA.Address_type IS NOT Null Then PA.Street2
-     Else Null
-       End Street2
-   ,Case
-     When SA.ADDRESS_TYPE = 'Seasonal' Then SA.Street3
-     When PA.Address_type IS NOT Null Then PA.Street3
-     Else Null
-       End Street3
-   ,Case
-     When SA.ADDRESS_TYPE = 'Seasonal' Then SA.Foreign_Cityzip
-     When PA.Address_type IS NOT Null Then PA.Foreign_Cityzip
-     Else Null
-       End Foreign_Zipcode
-   ,Case
-     When SA.ADDRESS_TYPE = 'Seasonal' Then SA.City
-     When PA.Address_type IS NOT Null Then PA.City
-     Else Null
-       End City
-  ,Case
-     When SA.ADDRESS_TYPE = 'Seasonal' Then SA.State_Code
-     When PA.Address_type IS NOT Null Then PA.State_Code
-     Else Null
-       End State
-  ,Case
-     When SA.ADDRESS_TYPE = 'Seasonal' Then SA.Zipcode
-     When PA.Address_type IS NOT Null Then PA.Zipcode
-     Else Null
-       End Zipcode
-  ,Case
-     When SA.ADDRESS_TYPE = 'Seasonal' Then SA.Country
-     When PA.Address_type IS NOT Null Then PA.Country
-     Else Null
-       End Country
-  ,SPH.*
-  ,E.Pref_Name_Sort
-FROM ENTITY E
-   INNER JOIN HH_End_Trans Trans
-   ON E.ID_NUMBER = Trans.HOUSEHOLD_ID
-   LEFT JOIN rpt_pbh634.v_entity_ksm_degrees D
-   ON Trans.HOUSEHOLD_ID = D."ID_NUMBER"
-   LEFT JOIN HH_End_Donors Don
-   ON Trans.HOUSEHOLD_ID = Don.HOUSEHOLD_ID
-   LEFT JOIN PrefAddress PA
-   ON E.ID_NUMBER = PA.ID_NUMBER
-   LEFT JOIN Seas_Address SA
-   ON E.ID_NUMBER = SA.ID_NUMBER
-   LEFT JOIN table(rpt_pbh634.ksm_pkg.tbl_special_handling_concat) SPH
-   ON E.ID_NUMBER = SPH.ID_NUMBER
-   LEFT JOIN Spouse S
-   ON E.ID_number = S.ID_NUMBER
-   LEFT JOIN Sally_Salut PSAL
-   ON E.ID_number = PSAL.ID_Number
-   LEFT JOIN Sally_Salut SSAL
-   ON E.spouse_ID_Number = SSAL.ID_Number
-   LEFT JOIN Prospect_Manager PM
-   ON E.ID_Number = PM.ID_Number
-   LEFT JOIN Prog_Prospect_Manager PP
-   ON E.ID_Number = PP.ID_Number
-   -- This is the first tab of people who do not have special handling
-   WHERE E.RECORD_STATUS_CODE = 'A'
-   AND SPH.NO_CONTACT IS NULL
-   AND SPH.NO_MAIL_IND IS NULL
+  , Case
+      When sa.address_type = 'Seasonal'
+        Then 'Seasonal'
+      When PA.Address_type Is Not Null
+        Then 'Preferred'
+      Else Null
+      End
+    As addr_type
+  , Case
+      When sa.address_type = 'Seasonal'
+        Then sa.company_name_1
+      When pa.address_type Is Not Null
+        Then pa.company_name_1
+      Else Null
+      End
+    As company_name_1
+  , Case
+      When sa.address_type = 'Seasonal'
+        Then sa.company_name_2
+      When pa.address_type Is Not Null
+        Then pa.company_name_2
+      Else Null
+      End
+    As company_name_2
+  , Case
+      When sa.address_type = 'Seasonal'
+        Then sa.street1
+      When pa.address_type Is Not Null
+        Then pa.street1
+      Else Null
+      End
+    As street1
+  , Case
+      When sa.address_type = 'Seasonal'
+        Then sa.street2
+      When pa.address_type Is Not Null
+        Then pa.street2
+      Else Null
+      End
+    As street2
+  , Case
+      When sa.address_type = 'Seasonal'
+        Then sa.street3
+      When pa.address_type Is Not Null
+        Then pa.street3
+      Else Null
+      End
+    As street3
+  , Case
+      When sa.address_type = 'Seasonal'
+        Then sa.foreign_cityzip
+      When pa.address_type Is Not Null
+        Then pa.foreign_cityzip
+      Else Null
+      End
+    As foreign_zipcode
+  , Case
+      When sa.address_type = 'Seasonal'
+        Then sa.city
+      When pa.address_type Is Not Null
+        Then pa.city
+      Else Null
+      End
+    As city
+  , Case
+      When sa.address_type = 'Seasonal'
+        Then sa.state_code
+      When pa.address_type Is Not Null
+        Then pa.state_code
+      Else Null
+      End
+    As state
+  , Case
+      When sa.address_type = 'Seasonal'
+        Then sa.zipcode
+      When pa.address_type Is Not Null
+        Then pa.zipcode
+      Else null
+      End
+    As zipcode
+  , Case
+      When sa.address_type = 'Seasonal'
+        Then sa.country
+      When pa.address_type Is Not Null
+        Then pa.country
+      Else Null
+      End
+    As country
+  , sph.*
+  , e.pref_name_sort
+From entity e
+  Inner join hh_end_trans trans
+    On e.id_number = trans.household_id
+  Left Join rpt_pbh634.v_entity_ksm_degrees d
+    On trans.household_id = d."ID_NUMBER"
+  Left Join hh_end_donors don
+    On trans.household_id = don.household_id
+  Left Join prefaddress pa
+    On e.id_number = pa.id_number
+  Left Join seas_address sa
+    On e.id_number = sa.id_number
+  Left Join table(rpt_pbh634.ksm_pkg.tbl_special_handling_concat) sph
+    On e.id_number = sph.id_number
+  Left Join spouse s
+    On e.id_number = s.id_number
+  Left Join sally_salut psal
+    On e.id_number = psal.id_number
+  Left Join sally_salut ssal
+    On e.spouse_id_number = ssal.id_number
+  Left Join prospect_manager pm
+    On e.id_number = pm.id_number
+  Left Join prog_prospect_manager pp
+    On e.id_number = pp.id_number
+  -- This is the first tab of people who do not have special handling
+  Where e.record_status_code = 'A'
+    And sph.no_contact Is Null
+    And sph.no_mail_ind Is Null
 
 -- Swap this in to generate the second tab, which is people with spec hand exclusions
 /* For the people that have been removed from Mailing 
