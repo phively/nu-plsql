c797f0b820d1a7c1145b2d813eb4d4d6cc3d0770
Paul Hively <pbhively@gmail.com>
2019-05-14 16:13:04 -0500

    KSM pledge balance view
    - KSM pledge balances for all active pledges, split by allocation
    - Added overall pledge summary stats: pledge balance, total, total to KSM, attributed donors, allocations
    - Pledge schedule amounts for current and next fiscal year
    - Most recent payment date and amount per allocation

--- views/v_ksm_pledge_balances.sql
+++ views/v_ksm_pledge_balances.sql
@@ -1,17 +1,9 @@
--- Create Or Replace View v_ksm_pledge_balances As
+Create Or Replace View v_ksm_pledge_balances As
 
 With
 
-/** Parameters - EDIT THIS **/
-params As (
-  Select
-    '20170901' As param_start_dt
-    , '20180831' As param_stop_dt
-  From DUAL
-)
-
 -- Transaction and pledge TMS table definition
-, tms_trans As (
+tms_trans As (
   (
     Select
       transaction_type_code
@@ -61,18 +53,27 @@ params As (
 , pledge_counts As (
   Select
     pledge.pledge_pledge_number
+    , max(plgd.prim_pledge_remaining_balance) As pledge_balance
+    , sum(pledge.pledge_amount)
+      As pledge_total
+    , sum(Case When allocation.alloc_school = 'KM' Then pledge.pledge_amount Else 0 End)
+      As pledge_total_ksm
     , count(Distinct pledge.pledge_allocation_name)
-      As allocs_count
+      As pledge_allocs
     , count(Distinct pledge.pledge_donor_id) - 1 -- Subtract 1 for the legal donor
-      As attr_donors_count
+      As pledge_addl_donors
   From pledge
   Inner Join ksm_pledges
     On ksm_pledges.pledge_pledge_number = pledge.pledge_pledge_number
+  Inner Join allocation
+    On allocation.allocation_code = pledge.pledge_allocation_name
+  Inner Join table(ksm_pkg.plg_discount) plgd
+    On plgd.pledge_number = pledge.pledge_pledge_number
   Group By pledge.pledge_pledge_number
 )
 
 -- Associated donors
-, plg_assoc_dnrs As (
+/*, plg_assoc_dnrs As (
   Select
     pledge.pledge_pledge_number
     , pledge.pledge_donor_id
@@ -90,7 +91,7 @@ params As (
       As inst_suffixes
   From plg_assoc_dnrs
   Group By pledge_pledge_number
-)
+)*/
 
 -- Pledge payments
 , ksm_payments As (
@@ -156,49 +157,14 @@ params As (
     ksm_pledges.pledge_pledge_number
     , psched.payment_schedule_status
     , psched.payment_schedule_date
-    , Case
-        When payment_schedule_date Not Like '0000%'
-          And payment_schedule_date Not Like '____00%'
-          Then rpt_pbh634.ksm_pkg.get_fiscal_year(to_date(psched.payment_schedule_date, 'YYYYMMDD'))
-        End
-      As payment_schedule_fiscal_year
+    , rpt_pbh634.ksm_pkg.get_fiscal_year(rpt_pbh634.ksm_pkg.to_date2(psched.payment_schedule_date, 'YYYYMMDD'))
+      As pay_sch_fy
     , psched.payment_schedule_amount
     , psched.payment_schedule_balance
   From payment_schedule psched
   Inner Join ksm_pledges
     On ksm_pledges.pledge_pledge_number = psched.payment_schedule_pledge_nbr
 )
-, pay_last As (
-  Select
-    pledge_pledge_number
-    , min(payment_schedule_date) Keep (dense_rank First Order By payment_schedule_date Desc, payment_schedule_balance Desc)
-      As last_sched_date_paid
-    , min(payment_schedule_fiscal_year) Keep (dense_rank First Order By payment_schedule_date Desc, payment_schedule_balance Desc)
-      As last_sched_year_paid
-    , min(payment_schedule_amount) Keep (dense_rank First Order By payment_schedule_date Desc, payment_schedule_balance Desc)
-      As last_sched_amount
-    , min(payment_schedule_balance) Keep (dense_rank First Order By payment_schedule_date Desc, payment_schedule_balance Desc)
-      As last_sched_balance
-  From pay_sch
-  Where payment_schedule_status = 'P'
-  Group By pledge_pledge_number
-)
-, pay_next As (
-  Select
-    pledge_pledge_number
-    , min(payment_schedule_date) Keep (dense_rank First Order By payment_schedule_date Asc, payment_schedule_balance Desc)
-      As next_sched_date
-    , min(payment_schedule_fiscal_year) Keep (dense_rank First Order By payment_schedule_date Asc, payment_schedule_balance Desc)
-      As next_sched_year
-    , min(payment_schedule_amount) Keep (dense_rank First Order By payment_schedule_date Asc, payment_schedule_balance Desc)
-      As next_sched_amount
-    , min(payment_schedule_balance) Keep (dense_rank First Order By payment_schedule_date Asc, payment_schedule_balance Desc)
-      As next_sched_balance
-  From pay_sch
-  Where payment_schedule_status = 'U'
-  Group By pledge_pledge_number
-)
-
 -- Counts
 , plg As (
   Select
@@ -220,16 +186,34 @@ params As (
   Select
     plg.id
     , plg.plg
-    , count(sc.payment_schedule_date) As scheduled_payments
-    , sum(Case When sc.payment_schedule_status = 'P' Then sc.payment_schedule_amount Else 0 End)
-      As paid_fy18
-    , sum(plg.prop * sc.payment_schedule_balance)
-      As pay      
-  From payment_schedule sc
+    , count(Case When sc.pay_sch_fy = cal.curr_fy Then sc.payment_schedule_date End)
+      As scheduled_payments_cfy
+    , sum(Case When sc.payment_schedule_status = 'P'
+            And sc.pay_sch_fy = cal.curr_fy
+            Then sc.payment_schedule_amount Else 0 End)
+      As paid_cfy
+    , sum(plg.prop * Case When sc.pay_sch_fy = cal.curr_fy Then sc.payment_schedule_balance Else 0 End)
+      As balance_cfy
+    , count(Case When sc.pay_sch_fy = cal.curr_fy + 1 Then sc.payment_schedule_date End)
+      As scheduled_payments_nfy1
+    , sum(Case When sc.payment_schedule_status = 'P'
+            And sc.pay_sch_fy = cal.curr_fy + 1
+            Then sc.payment_schedule_amount Else 0 End)
+      As paid_nfy1
+    , sum(plg.prop * Case When sc.pay_sch_fy = cal.curr_fy + 1 Then sc.payment_schedule_balance Else 0 End)
+      As balance_nfy1
+    , count(Case When sc.pay_sch_fy = cal.curr_fy - 1 Then sc.payment_schedule_date End)
+      As scheduled_payments_pfy1
+    , sum(Case When sc.payment_schedule_status = 'P'
+            And sc.pay_sch_fy = cal.curr_fy - 1
+            Then sc.payment_schedule_amount Else 0 End)
+      As paid_pfy1
+    , sum(plg.prop * Case When sc.pay_sch_fy = cal.curr_fy - 1 Then sc.payment_schedule_balance Else 0 End)
+      As balance_pfy1
+  From pay_sch sc
+  Cross Join v_current_calendar cal
   Inner Join plg
-    On plg.plg = sc.payment_schedule_pledge_nbr
-  Cross Join params
-  Where sc.payment_schedule_date Between param_start_dt And param_stop_dt
+    On plg.plg = sc.pledge_pledge_number
   Group By
     plg.id
     , plg.plg
@@ -246,29 +230,43 @@ Select
   , p.pledge_pledge_number As pledge_number
   , pp.prim_pledge_date_of_record As date_of_record
   , pp.prim_pledge_year_of_giving As fiscal_year
+  , pledge_counts.pledge_total
+  , pledge_counts.pledge_total_ksm
+  , pledge_counts.pledge_balance
+  , pledge_counts.pledge_allocs
+  , pledge_counts.pledge_addl_donors
+  -- Scheduled payments
+  , cp.scheduled_payments_cfy
+  , cp.paid_cfy
+  , cp.balance_cfy
+  , cp.scheduled_payments_nfy1
+  , cp.paid_nfy1
+  , cp.balance_nfy1
   -- Allocation fields
-  , p2.pledge_allocation_name As allocation_code
+  , p.pledge_allocation_name As allocation_code
   , alloc.short_name As allocation_name
   , Case When ksm_af_allocs.af_flag Is Not Null Then 'Y' End
     As ksm_cru_flag
   , Case When ksm_af_allocs.af_flag = 'Y' Then 'Y' End
     As ksm_af_flag
   -- Amount fields
-  , p2.pledge_amount As alloc_pledge_amount
-  , ksm_paid_amt.total_paid As alloc_total_paid
-  , p2.pledge_amount - nvl(ksm_paid_amt.total_paid, 0) As alloc_pledge_balance
-  , cp.scheduled_payments As scheduled_payments_fy18
-  , cp.paid_fy18
-  , cp.pay As balance_fy18
+  , p.pledge_amount As alloc_pledge_amount
+  , nvl(ksm_paid_amt.total_paid, 0) As alloc_total_paid
+  , p.pledge_amount - nvl(ksm_paid_amt.total_paid, 0) As alloc_pledge_balance
+  -- Recent payments
+  , pmtsa.recent_pmt_nbr_alloc
+  , pmtsa.date_of_record_alloc
+  , pmtsa.pmt_amount_alloc
 From entity e
 Inner Join pledge p
-  on e.id_number = p.pledge_donor_id
-Inner Join pledge p2
-  On p2.pledge_pledge_number = p.pledge_pledge_number
-  And p2.pledge_associated_code In ('P', 'S')
-  And p2.pledge_alloc_school = 'KM'
+  On e.id_number = p.pledge_donor_id
+  And p.pledge_associated_code In ('P', 'S')
+  And p.pledge_alloc_school = 'KM'
+-- Pledge summary
 Inner Join primary_pledge pp 
   On pp.prim_pledge_number = p.pledge_pledge_number
+Inner Join pledge_counts
+  On pledge_counts.pledge_pledge_number = p.pledge_pledge_number
 -- Descriptions from codes
 Inner Join tms_trans 
   On tms_trans.transaction_type_code = pp.prim_pledge_type
@@ -298,15 +296,11 @@ Left Join recent_payments pmts
 Left Join recent_payments_alloc pmtsa
   On pmtsa.pmt_on_pledge_number = p.pledge_pledge_number
   And pmtsa.allocation_code = p.pledge_allocation_name
--- Payment schedule
-Left Join pay_last
-  On pay_last.pledge_pledge_number = p.pledge_pledge_number
-Left Join pay_next
-  On pay_next.pledge_pledge_number = p.pledge_pledge_number
 -- Counts
-Inner Join count_pledges cp 
- On e.id_number = cp.id
- And p.pledge_pledge_number = cp.plg
+Inner Join count_pledges cp
+  On e.id_number = cp.id
+  And p.pledge_pledge_number = cp.plg
 Where e.record_status_code Not In ('I','X','D')
   -- Recurring gift, straight pledge, NBI, grant pledge only
   And transaction_type_code In ('RC', 'ST', 'NB', 'GP')
+;
