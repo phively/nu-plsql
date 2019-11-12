3f615aa12d0e10b416dd3c15275da927af449590
Paul Hively <pbhively@gmail.com>
2019-11-12 10:45:27 -0600

    KSM datamart: formatting pass

--- views/v_ksm_data_mart.sql
+++ views/v_ksm_data_mart.sql
@@ -10,167 +10,172 @@ Conventions:
 - Always include date added and modified in the disaggregated data views
 ************************************************************************/
 
-Create or Replace View v_datamart_interests As
+Create Or Replace View v_datamart_interests As
+-- View of INTEREST (Alumni List) v-datamart_interests
+Select
+  interest.id_number As catracks_id
+  , tms_interest.short_desc AS interest_desc
+  , interest.interest_code As interest_code
+  , interest.start_dt
+  , rpt_pbh634.ksm_pkg.to_date2(start_dt) As interest_start_date
+  , interest.stop_dt
+  , rpt_pbh634.ksm_pkg.to_date2(stop_dt) As interest_stop_date
+  , interest.date_added
+  , interest.date_modified
+From interest 
+Inner Join tms_interest
+  On tms_interest.interest_code = interest.interest_code --- Produce TMS Codes
+Inner Join rpt_pbh634.v_entity_ksm_degrees deg
+  On deg.id_number = interest.id_number --- Only Kellogg Alumni
+Where tms_interest.interest_code Like 'L%' --- Any Linkedin Industry Code
+  Or tms_Interest.interest_code = '16'  --- KIS also wants the "16" Research Code
+Order By interest_code Asc
+;
 
-
-Select interest.id_Number AS catracks_id,
-       tms_interest.short_desc AS interest_desc,
-       interest.interest_Code As interest_code,
-       interest.start_dt,
-       rpt_pbh634.ksm_pkg.to_date2(start_dt) As interest_start_date,
-       interest.stop_dt,
-       rpt_pbh634.ksm_pkg.to_date2(stop_dt) As interest_stop_date,
-       interest.date_added,
-       interest.date_modified
-From Interest 
-Inner JOIN tms_interest ON tms_interest.interest_code = interest.interest_code --- Produce TMS Codes
-Inner Join rpt_pbh634.v_entity_ksm_degrees deg on deg.id_number = interest.id_number --- Only Kellogg Alumni
-Where tms_interest.interest_code LIKE 'L%' --- Any Linkedin Industry Code
-or tms_Interest.interest_code = '16'  --- KIS also wants the "16" Research Code 
-Order By interest_code ASC;
-
-
---- View of KSM alumni with least a EMPLID, SES, or NETID along with a Catracks ID: v_datamart_ids 
-
-
-Create or Replace View v_datamart_ids AS 
- 
-
-With KSM_IDS AS (select ids_base.id_number,
-
-       ids_base.ids_type_code,
-
-       ids_base.other_id
-
-From rpt_pbh634.v_entity_ksm_degrees deg --- Kellogg Alumni Only
-
-Left Join ids_base on ids_Base.id_number = deg.id_number
-
-Where ids_base.ids_type_code IN('SES', 'KSF','NET')) --- SES = EMPLID + KSF = Salesforce ID + NET = NetID
+Create Or Replace View v_datamart_ids As
+-- View of KSM alumni with least a EMPLID, SES, or NETID along with a Catracks ID: v_datamart_ids
+With ksm_ids As (
+  Select ids_base.id_number
+    , ids_base.ids_type_code
+    , ids_base.other_id
+  From rpt_pbh634.v_entity_ksm_degrees deg --- Kellogg Alumni Only
+  Left Join ids_base
+    On ids_base.id_number = deg.id_number
+  Where ids_base.ids_type_code In ('SES', 'KSF','NET') --- SES = EMPLID + KSF = Salesforce ID + NET = NetID
+) 
 
 Select Distinct
-KSM_ids.id_number As catracks_id
-, ses.other_id As emplid
-, ksf.other_id As salesforce_id
-, net.other_id As netid
+  ksm_ids.id_number As catracks_id
+  , ses.other_id As emplid
+  , ksf.other_id As salesforce_id
+  , net.other_id As netid
 From ksm_ids
 Inner Join rpt_pbh634.v_entity_ksm_degrees deg
-     On deg.id_number = ksm_ids.id_number
+  On deg.id_number = ksm_ids.id_number
 Left Join ksm_ids ses
-     On ses.id_number = ksm_ids.id_number
-     And ses.ids_type_code = 'SES'
+  On ses.id_number = ksm_ids.id_number
+  And ses.ids_type_code = 'SES'
 Left Join ksm_ids KSF
-     On ksf.id_number = ksm_ids.id_number
-     And ksf.ids_type_code = 'KSF'
+  On ksf.id_number = ksm_ids.id_number
+  And ksf.ids_type_code = 'KSF'
 Left Join ksm_ids net
-     On net.id_number = ksm_ids.id_number
-     And net.ids_type_code = 'NET'; --- Selects IDs for each row
+  On net.id_number = ksm_ids.id_number
+  And net.ids_type_code = 'NET' --- Selects IDs for each row
+;
 
+Create Or Replace View v_datamart_address As
+-- View for Address (Business + Home) v_data_mart_address
+With business_address As (
+  Select Distinct
+    address.id_number
+    , address.city
+    , address.state_code
+    , address.country_code
+    , address.addr_type_code
+    , address.addr_status_code
+    , rpt_pbh634.v_geo_code_primary.geo_codes
+    , rpt_pbh634.v_geo_code_primary.geo_code_primary
+    , rpt_pbh634.v_geo_code_primary.geo_code_primary_desc
+  From Address
+  Left Join rpt_pbh634.v_geo_code_primary
+    On rpt_pbh634.v_geo_code_primary.id_number = address.id_number
+    And rpt_pbh634.v_geo_code_primary.xsequence = address.xsequence --- Joining Paul's New Geocode Table to get Business Address Geocodes 
+  Where address.addr_type_code = 'B'
+  and Address.Addr_Status_Code = 'A'
+)
 
---- View for Address (Business + Home) v_data_mart_address 
-
-
-Create or Replace View v_datamart_address AS
-
-
-With Business_Address As (
-
-Select Distinct 
-Address.Id_Number,
-Address.City,
-Address.State_Code,
-Address.Country_Code,
-Address.Addr_Type_Code,
-Address.Addr_Status_Code,
-rpt_pbh634.v_geo_code_primary.GEO_CODES,
-rpt_pbh634.v_geo_code_primary.GEO_CODE_PRIMARY,
-rpt_pbh634.v_geo_code_primary.GEO_CODE_PRIMARY_DESC
-
-From Address
-Left Join rpt_pbh634.v_geo_code_primary on rpt_pbh634.v_geo_code_primary.ID_NUMBER = address.ID_NUMBER
-And rpt_pbh634.v_geo_code_primary.XSEQUENCE = address.XSEQUENCE --- Joining Paul's New Geocode Table to get Business Address Geocodes 
-Where Address.addr_type_code = 'B'
-and Address.Addr_Status_Code = 'A'),
-
-Home_Address As (
-
-Select Distinct 
-Address.Id_Number,
-Address.City,--- KIS Wants Homes
-Address.State_Code,
-Address.Country_Code,
-Address.Addr_Type_Code,
-Address.Addr_Status_Code,
-rpt_pbh634.v_geo_code_primary.GEO_CODES,--- KIS Wants Geocodes Home Address
-rpt_pbh634.v_geo_code_primary.GEO_CODE_PRIMARY,
-rpt_pbh634.v_geo_code_primary.GEO_CODE_PRIMARY_DESC
-
-From Address
-Left Join rpt_pbh634.v_geo_code_primary on rpt_pbh634.v_geo_code_primary.ID_NUMBER = address.ID_NUMBER
-And rpt_pbh634.v_geo_code_primary.XSEQUENCE = address.XSEQUENCE --- Joining Paul's New Geocode Table to get Business Address Geocodes 
-Where Address.addr_type_code = 'H'
-and Address.Addr_Status_Code = 'A')
-
+, home_address As (
+  Select Distinct
+    address.id_number
+    , address.city --- KIS Wants Homes
+    , address.state_code
+    , address.country_code
+    , address.addr_type_code
+    , address.addr_status_code
+    , rpt_pbh634.v_geo_code_primary.geo_codes --- KIS Wants Geocodes Home Address
+    , rpt_pbh634.v_geo_code_primary.geo_code_primary
+    , rpt_pbh634.v_geo_code_primary.geo_code_primary_desc
+  From Address
+  Left Join rpt_pbh634.v_geo_code_primary
+    On rpt_pbh634.v_geo_code_primary.id_number = address.id_number
+    And rpt_pbh634.v_geo_code_primary.xsequence = address.xsequence --- Joining Paul's New Geocode Table to get Business Address Geocodes
+  Where address.addr_type_code = 'H'
+    and address.addr_status_code = 'A'
+)
 
 Select Distinct 
-address.id_number As Catracks_ID,
-       home_address.city AS home_city, 
-       home_address.state_code AS home_state,
-       home_address.country_code AS home_country,
-       home_address.GEO_CODES AS home_geo_codes,
-       home_address.GEO_CODE_PRIMARY AS home_geo_primary_code, 
-       home_address.GEO_CODE_PRIMARY_DESC AS home_geo_primary_desc,
-       Business_Address.City AS business_city,
-       Business_Address.State_Code AS business_state,
-       Business_Address.Country_Code AS business_country_code,
-       tms_country.short_desc As business_country,
-       Business_Address.GEO_CODES AS business_geo_code, --- Kis Wants Geocodes for Business Address
-       Business_Address.GEO_CODE_PRIMARY As business_geo_primary_code,
-       Business_Address.GEO_CODE_PRIMARY_DESC AS business_geo_primary_desc
+  address.id_number As catracks_id
+  , home_address.city As home_city
+  , home_address.state_code As home_state
+  , home_address.country_code As home_country
+  , home_address.geo_codes As home_geo_codes
+  , home_address.geo_code_primary As home_geo_primary_code
+  , home_address.geo_code_primary_desc As home_geo_primary_desc
+  , business_address.city As business_city
+  , business_address.state_code As business_state
+  , business_address.country_code As business_country_code
+  , tms_country.short_desc As business_country
+  , business_address.geo_codes As business_geo_code --- Kis Wants Geocodes for Business Address
+  , business_address.geo_code_primary As business_geo_primary_code
+  , business_address.geo_code_primary_desc As business_geo_primary_desc
 From address 
-Inner Join rpt_pbh634.v_entity_ksm_degrees deg on deg.id_number = address.id_number --- Degrees Table Base to only get KSM Alumni
-Left Join address ON deg.ID_NUMBER = address.id_number --- Joining Address Table to get Business Address
-Left Join Business_Address ON Business_Address.Id_Number = deg.ID_NUMBER --- Join Subquery for Business Address
-Left Join Home_Address ON Home_Address.id_number = deg.ID_NUMBER --- Join Subquery for Home Address
-Left Join tms_country ON business_address.Country_Code = tms_country.country_code --- Join to get Home Country Description
-Order By address.ID_NUMBER ASC;
-
-
---- View for Employer: v_data_mart_employer
+Inner Join rpt_pbh634.v_entity_ksm_degrees deg
+  On deg.id_number = address.id_number --- Degrees Table Base to only get KSM Alumni
+Left Join address
+  On deg.id_number = address.id_number --- Joining Address Table to get Business Address
+Left Join business_address
+  On business_address.id_number = deg.id_number --- Join Subquery for Business Address
+Left Join home_address
+  On home_address.id_number = deg.id_number --- Join Subquery for Home Address
+Left Join tms_country 
+  On business_address.country_code = tms_country.country_code --- Join to get Home Country Description
+Order By address.id_number Asc
+;
 
 Create or Replace View v_datamart_employment AS
+--- View for Employer: v_data_mart_employer
+With org_employer As (
+  --- Using subquery to Get Employer Names from Employee ID #'s 
+  Select id_number, report_name
+  From entity 
+  Where entity.person_or_org = 'O'
+) 
 
-With org_employer As 
-(Select id_number, report_name
-From entity 
-Where entity.person_or_org = 'O') --- Using subquery to Get Employer Names from Employee ID #'s 
-
-Select employ.id_Number AS catracks_id,
-       employ.start_dt As start_date,
-       rpt_pbh634.ksm_pkg.to_date2 (employ.start_dt) As employment_start_date,
-       employ.Stop_Dt As stop_date,
-       rpt_pbh634.ksm_pkg.to_date2 (employ.Stop_Dt) As employment_stop_date,
-       employ.job_status_code As job_status_code,
-       tms_job_status.short_desc AS job_status_desc,
-       employ.primary_emp_Ind AS primary_employer_indicator,
-       employ.self_employ_Ind As self_employed_indicator,
-       employ.job_title,
-       (Case When Employ.Employer_Name1 = ' ' then org_employer.report_name Else Employ.Employer_Name1 End) As Employer, --- Used for those alumni with an employer code, but not employer name1
-       employ.fld_of_work_code As fld_of_work_code,
-       fow.short_desc AS Fld_of_work_desc,
-       employ.date_added,
-       employ.date_modified,
-       employ.operator_name
-       
-
+Select
+  employ.id_Number As catracks_id
+  , employ.start_dt As start_date
+  , rpt_pbh634.ksm_pkg.to_date2(employ.start_dt) As employment_start_date
+  , employ.Stop_Dt As stop_date
+  , rpt_pbh634.ksm_pkg.to_date2(employ.stop_dt) As employment_stop_date
+  , employ.job_status_code As job_status_code
+  , tms_job_status.short_desc As job_status_desc
+  , employ.primary_emp_ind As primary_employer_indicator
+  , employ.self_employ_ind As self_employed_indicator
+  , employ.job_title
+  , Case --- Used for those alumni with an employer code, but not employer name1
+      When employ.employer_name1 = ' '
+        Then org_employer.report_name
+      Else employ.employer_name1
+      End
+    As employer
+  , employ.fld_of_work_code As fld_of_work_code
+  , fow.short_desc As fld_of_work_desc
+  , employ.date_added
+  , employ.date_modified
+  , employ.operator_name
 From employment employ
-Inner Join rpt_pbh634.v_entity_ksm_degrees deg on deg.ID_NUMBER = employ.id_number --- To get KSM alumni 
-Inner Join tms_fld_of_work fow on employ.fld_of_work_code = fow.fld_of_work_code --- To get FLD of Work Code
-Left  Join tms_job_status On tms_job_status.job_status_code = employ.job_status_code --- To get job description
-Left Join org_employer ON org_employer.id_number = employ.employer_id_number --- To get the name of those with employee ID
-Where employ.job_status_code IN ('C','P','Q','R', ' ', 'L')
+Inner Join rpt_pbh634.v_entity_ksm_degrees deg
+  On deg.id_number = employ.id_number --- To get KSM alumni 
+Inner Join tms_fld_of_work fow
+  On employ.fld_of_work_code = fow.fld_of_work_code --- To get FLD of Work Code
+Left  Join tms_job_status
+  On tms_job_status.job_status_code = employ.job_status_code --- To get job description
+Left Join org_employer
+  On org_employer.id_number = employ.employer_id_number --- To get the name of those with employee ID
+Where employ.job_status_code In ('C','P','Q','R', ' ', 'L')
 --- Employment Key: C = Current, P = Past, Q = Semi Retired R = Retired L = On Leave 
-Order By employ.id_Number ASC;
+Order By employ.id_Number Asc
+;
 
 Create Or Replace View v_datamart_degrees As
 -- KSM degrees view
@@ -202,7 +207,7 @@ Select
   , degrees.date_modified
   , degrees.operator_name
 From degrees
-Inner Join v_entity_ksm_degrees deg -- Alumni only
+Inner Join rpt_pbh634.v_entity_ksm_degrees deg -- Alumni only
   On deg.id_number = degrees.id_number
 Left Join institution
   On institution.institution_code = degrees.institution_code
@@ -241,7 +246,7 @@ Select
   -- Current business address info
   -- Current employment info
   -- Concatenated interests
-From v_entity_ksm_degrees deg
+From rpt_pbh634.v_entity_ksm_degrees deg
 Left Join tms_record_status tms_rs
   On tms_rs.record_status_code = deg.record_status_code
 Where deg.record_status_code In ('A', 'C', 'L', 'D')
