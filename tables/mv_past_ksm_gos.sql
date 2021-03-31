cf598111ecec55427975c9e7f0733bcd60095cd6
Paul Hively <pbhively@gmail.com>
2021-03-31 17:06:29 -0500

    Update staff views
    - Added new AG staff and start date to staff table

--- tables/mv_past_ksm_gos.sql
+++ tables/mv_past_ksm_gos.sql
@@ -59,6 +59,7 @@ adv_dates As (
   Union All Select '0000838571', 'AF', to_date('20201109', 'yyyymmdd'), to_date(NULL, 'yyyymmdd') From DUAL
   Union All Select '0000838656', 'MG', to_date('20201111', 'yyyymmdd'), to_date(NULL, 'yyyymmdd') From DUAL
   Union All Select '0000364856', 'MG', to_date('20201116', 'yyyymmdd'), to_date(NULL, 'yyyymmdd') From DUAL
+  Union All Select '0000841644', 'AF', to_date('20210315', 'yyyymmdd'), to_date(NULL, 'yyyymmdd') From DUAL
 )
 
 Select
