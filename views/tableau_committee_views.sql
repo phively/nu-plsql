8245d49394a36980ebc264da1d02149a797563de
Paul Hively <pbhively@gmail.com>
2021-10-27 10:22:16 -0500

    Advisory committee: flag refactoring
    - Moved all committee left joins into their own subquery for speed. The deduped committee table subquery now finishes in ~5 seconds.

--- views/tableau_committee_views.sql
+++ views/tableau_committee_views.sql
@@ -113,8 +113,142 @@ members As (
 )
 
 , all_committees As (
-  Select Distinct id_number
-  From members
+  Select Distinct
+    ac.id_number
+    , Case When gab.committee_code = 'U' Then 'Y' Else 'N' End
+      As gab_ind
+    , gab.start_dt As gab_start_date
+    , gab.stop_dt As gab_stop_date
+    , gab.status As gab_status
+    , gab.role As gab_role
+    , gab.committee_title As gab_committee_title
+    , Case When kfn.committee_code = 'KFN' Then 'Y' Else 'N' End
+      As kfn_ind
+    , kfn.start_dt As kfn_start_date
+    , kfn.stop_dt As kfn_stop_date
+    , kfn.status As kfn_status
+    , kfn.role As kfn_role
+    , kfn.committee_title As kfn_committee_title
+    , Case When corpgov.committee_code = 'KCGN' Then 'Y' Else 'N' End
+      As corpgov_ind
+    , corpgov.start_dt As corpgov_start_date
+    , corpgov.stop_dt As corpgov_stop_date
+    , corpgov.status As corpgov_status
+    , corpgov.role As corpgov_role
+    , corpgov.committee_title As corpgov_committee_title
+    , Case When divsummit.committee_code = 'KCDO' Then 'Y' Else 'N' End
+      As divsummit_ind
+    , divsummit.start_dt As ds_start_date
+    , divsummit.stop_dt As ds_stop_date
+    , divsummit.status As ds_status
+    , divsummit.role As ds_role
+    , divsummit.committee_title As divsummit_committee_title
+    , Case When RealEstCouncil.committee_code = 'KREAC' Then 'Y' Else 'N' End
+      As realest_ind
+    , realestcouncil.start_dt As rec_start_date
+    , realestcouncil.stop_dt As rec_stop_date
+    , realestcouncil.status As rec_status
+    , realestcouncil.role As rec_role
+    , realestcouncil.committee_title As rec_committee_title
+    , Case When amp.committee_code = 'KAMP' Then 'Y' Else 'N' End
+      As amp_ind
+    , amp.start_dt As amp_start_date
+    , amp.stop_dt As amp_stop_date
+    , amp.status As amp_status
+    , amp.role As amp_role
+    , amp.committee_title As amp_committee_title
+    , Case When healthcare.committee_code = 'HAK' Then 'Y' Else 'N' End
+      As healthcare_ind
+    , healthcare.start_dt As healthcare_start_date
+    , healthcare.stop_dt As healthcare_stop_date
+    , healthcare.status As healthcare_status
+    , healthcare.role As healthcare_role
+    , healthcare.committee_title As healthcare_committee_title
+    , Case When WomensLeadership.committee_code = 'KWLC' Then 'Y' Else 'N' End
+      As womensleadership_ind
+    , womensleadership.start_dt As womensleadership_start_date
+    , womensleadership.stop_dt As womensleadership_stop_date
+    , womensleadership.status As womensleadership_status
+    , womensleadership.role As womensleadership_role
+    , womensleadership.committee_title As womens_committee_title
+    , Case When KAC.committee_code = 'KACNA' Then 'Y' Else 'N' End
+      As kac_ind
+    , kac.start_dt As kac_start_date
+    , kac.stop_dt As kac_stop_date
+    , kac.status As kac_status
+    , kac.role As kac_role
+    , kac.committee_title As kac_committee_title
+    , Case When phs.committee_code = 'KPH' Then 'Y' Else 'N' End
+      As phs_ind
+    , phs.start_dt As phs_start_date
+    , phs.stop_dt As phs_stop_date
+    , phs.status As phs_status
+    , phs.role As phs_role
+    , phs.committee_title As phs_committee_title
+    , Case When inclusion.committee_code = 'KIC' Then 'Y' Else 'N' End
+      As inclusion_ind
+    , inclusion.start_dt As inclusion_start_date
+    , inclusion.stop_dt As inclusion_stop_date
+    , inclusion.status As inclusion_status
+    , inclusion.role As inclusion_role
+    , inclusion.committee_title As inclusion_committee_title
+    , Case When private_equity.committee_code = 'KPETC' Then 'Y' Else 'N' End
+      As private_equity_ind
+    , private_equity.start_dt As private_equity_start_date
+    , private_equity.stop_dt As private_equity_stop_date
+    , private_equity.status As private_equity_status
+    , private_equity.role As private_equity_role
+    , private_equity.committee_title As private_equity_committee_title
+    , Case When keba.committee_code = 'KEBA' Then 'Y' Else 'N' End
+      As keba_ind
+    , keba.start_dt As keba_start_date
+    , keba.stop_dt As keba_stop_date
+    , keba.status As keba_status
+    , keba.role As keba_role
+    , keba.committee_title As keba_committee_title
+  From members ac
+  Left Join members amp
+  On amp.id_number = ac.id_number
+  And amp.committee_short_desc = 'AMP'
+  Left Join members realestcouncil
+    On realestcouncil.id_number = ac.id_number
+    And realestcouncil.committee_short_desc = 'RealEstCouncil'
+  Left Join members divsummit
+    On divsummit.id_number = ac.id_number
+    And divsummit.committee_short_desc = 'DivSummit'
+  Left Join members womensummit
+    On womensummit.id_number = ac.id_number
+    And womensummit.committee_short_desc = 'WomenSummit'
+  Left Join members corpgov
+    On corpgov.id_number = ac.id_number
+    And corpgov.committee_short_desc = 'CorpGov'
+  Left Join members kfn
+    On kfn.id_number = ac.id_number
+    And kfn.committee_short_desc = 'KFN'
+  Left Join members gab
+    On gab.id_number = ac.id_number
+    And gab.committee_short_desc = 'GAB'
+  Left Join members healthcare
+    On healthcare.id_number = ac.id_number
+    And healthcare.committee_short_desc = 'Healthcare'
+  Left Join members WomensLeadership
+    On WomensLeadership.id_number = ac.id_number
+    And WomensLeadership.committee_short_desc = 'WomensLeadership'
+  Left Join members KAC
+    On KAC.id_number = ac.id_number
+    And KAC.committee_short_desc = 'KAC'
+  Left Join members PHS
+    On KAC.id_number = ac.id_number
+    And KAC.committee_short_desc = 'PHS'
+  Left Join members inclusion
+    On inclusion.id_number = ac.id_number
+    And inclusion.committee_short_desc = 'Inclusion'
+  Left Join members private_equity
+    On private_equity.id_number = ac.id_number
+    And private_equity.committee_short_desc = 'PrivateEquity'
+  Left Join members keba
+    On keba.id_number = ac.id_number
+    And keba.committee_short_desc = 'KEBA'
 )
 
 -- NU yearly NGC giving amounts
@@ -206,97 +340,85 @@ Select
   , prs.evaluation_rating
   , prs.evaluation_date
   , prs.officer_rating
-  , Case When gab.committee_code = 'U' Then 'Y' Else 'N' End
-    As gab_ind
-  , gab.start_dt As gab_start_date
-  , gab.stop_dt As gab_stop_date
-  , gab.status As gab_status
-  , gab.role As gab_role
-  , gab.committee_title As gab_committee_title
-  , Case When kfn.committee_code = 'KFN' Then 'Y' Else 'N' End
-    As kfn_ind
-  , kfn.start_dt As kfn_start_date
-  , kfn.stop_dt As kfn_stop_date
-  , kfn.status As kfn_status
-  , kfn.role As kfn_role
-  , kfn.committee_title As kfn_committee_title
-  , Case When corpgov.committee_code = 'KCGN' Then 'Y' Else 'N' End
-    As corpgov_ind
-  , corpgov.start_dt As corpgov_start_date
-  , corpgov.stop_dt As corpgov_stop_date
-  , corpgov.status As corpgov_status
-  , corpgov.role As corpgov_role
-  , corpgov.committee_title As corpgov_committee_title
-  , Case When divsummit.committee_code = 'KCDO' Then 'Y' Else 'N' End
-    As divsummit_ind
-  , divsummit.start_dt As ds_start_date
-  , divsummit.stop_dt As ds_stop_date
-  , divsummit.status As ds_status
-  , divsummit.role As ds_role
-  , divsummit.committee_title As divsummit_committee_title
-  , Case When RealEstCouncil.committee_code = 'KREAC' Then 'Y' Else 'N' End
-    As realest_ind
-  , realestcouncil.start_dt As rec_start_date
-  , realestcouncil.stop_dt As rec_stop_date
-  , realestcouncil.status As rec_status
-  , realestcouncil.role As rec_role
-  , realestcouncil.committee_title As rec_committee_title
-  , Case When amp.committee_code = 'KAMP' Then 'Y' Else 'N' End
-    As amp_ind
-  , amp.start_dt As amp_start_date
-  , amp.stop_dt As amp_stop_date
-  , amp.status As amp_status
-  , amp.role As amp_role
-  , amp.committee_title As amp_committee_title
-  , Case When healthcare.committee_code = 'HAK' Then 'Y' Else 'N' End
-    As healthcare_ind
-  , healthcare.start_dt As healthcare_start_date
-  , healthcare.stop_dt As healthcare_stop_date
-  , healthcare.status As healthcare_status
-  , healthcare.role As healthcare_role
-  , healthcare.committee_title As healthcare_committee_title
-  , Case When WomensLeadership.committee_code = 'KWLC' Then 'Y' Else 'N' End
-    As womensleadership_ind
-  , womensleadership.start_dt As womensleadership_start_date
-  , womensleadership.stop_dt As womensleadership_stop_date
-  , womensleadership.status As womensleadership_status
-  , womensleadership.role As womensleadership_role
-  , womensleadership.committee_title As womens_committee_title
-  , Case When KAC.committee_code = 'KACNA' Then 'Y' Else 'N' End
-    As kac_ind
-  , kac.start_dt As kac_start_date
-  , kac.stop_dt As kac_stop_date
-  , kac.status As kac_status
-  , kac.role As kac_role
-  , kac.committee_title As kac_committee_title
-  , Case When phs.committee_code = 'KPH' Then 'Y' Else 'N' End
-    As phs_ind
-  , phs.start_dt As phs_start_date
-  , phs.stop_dt As phs_stop_date
-  , phs.status As phs_status
-  , phs.role As phs_role
-  , phs.committee_title As phs_committee_title
-  , Case When inclusion.committee_code = 'KIC' Then 'Y' Else 'N' End
-    As inclusion_ind
-  , inclusion.start_dt As inclusion_start_date
-  , inclusion.stop_dt As inclusion_stop_date
-  , inclusion.status As inclusion_status
-  , inclusion.role As inclusion_role
-  , inclusion.committee_title As inclusion_committee_title
-  , Case When private_equity.committee_code = 'KPETC' Then 'Y' Else 'N' End
-    As private_equity_ind
-  , private_equity.start_dt As private_equity_start_date
-  , private_equity.stop_dt As private_equity_stop_date
-  , private_equity.status As private_equity_status
-  , private_equity.role As private_equity_role
-  , private_equity.committee_title As private_equity_committee_title
-  , Case When keba.committee_code = 'KEBA' Then 'Y' Else 'N' End
-    As keba_ind
-  , keba.start_dt As keba_start_date
-  , keba.stop_dt As keba_stop_date
-  , keba.status As keba_status
-  , keba.role As keba_role
-  , keba.committee_title As keba_committee_title
+  -- Committee flags
+  , gab_ind
+  , gab_start_date
+  , gab_stop_date
+  , gab_status
+  , gab_role
+  , gab_committee_title
+  , kfn_ind
+  , kfn_start_date
+  , kfn_stop_date
+  , kfn_status
+  , kfn_role
+  , kfn_committee_title
+  , corpgov_ind
+  , corpgov_start_date
+  , corpgov_stop_date
+  , corpgov_status
+  , corpgov_role
+  , corpgov_committee_title
+  , divsummit_ind
+  , ds_start_date
+  , ds_stop_date
+  , ds_status
+  , ds_role
+  , divsummit_committee_title
+  , realest_ind
+  , rec_start_date
+  , rec_stop_date
+  , rec_status
+  , rec_role
+  , rec_committee_title
+  , amp_ind
+  , amp_start_date
+  , amp_stop_date
+  , amp_status
+  , amp_role
+  , amp_committee_title
+  , healthcare_ind
+  , healthcare_start_date
+  , healthcare_stop_date
+  , healthcare_status
+  , healthcare_role
+  , healthcare_committee_title
+  , womensleadership_ind
+  , womensleadership_start_date
+  , womensleadership_stop_date
+  , womensleadership_status
+  , womensleadership_role
+  , womens_committee_title
+  , kac_ind
+  , kac_start_date
+  , kac_stop_date
+  , kac_status
+  , kac_role
+  , kac_committee_title
+  , phs_ind
+  , phs_start_date
+  , phs_stop_date
+  , phs_status
+  , phs_role
+  , phs_committee_title
+  , inclusion_ind
+  , inclusion_start_date
+  , inclusion_stop_date
+  , inclusion_status
+  , inclusion_role
+  , inclusion_committee_title
+  , private_equity_ind
+  , private_equity_start_date
+  , private_equity_stop_date
+  , private_equity_status
+  , private_equity_role
+  , private_equity_committee_title
+  , keba_ind
+  , keba_start_date
+  , keba_stop_date
+  , keba_status
+  , keba_role
+  , keba_committee_title
   , nvl(prs.giving_total, 0) As nu_lt_giving
   , acg.cfy_nult_giving
   , acg.lfy_nult_giving
@@ -314,55 +436,15 @@ Select
   , acg.campaign_pfy1
   , acg.campaign_pfy2
   , acg.campaign_pfy3
-From all_committees_giving acg
+From all_committees ac
 Inner Join nu_prs_trp_prospect prs
-  On prs.id_number = acg.id_number
+  On prs.id_number = ac.id_number
 Left Join rpt_pbh634.v_entity_ksm_degrees
-  On v_entity_ksm_degrees.id_number = acg.id_number
+  On v_entity_ksm_degrees.id_number = ac.id_number
 Left Join proposalcount
   On proposalcount.prospect_id = prs.prospect_id
-Left Join members amp
-  On amp.id_number = acg.id_number
-  And amp.committee_short_desc = 'AMP'
-Left Join members realestcouncil
-  On realestcouncil.id_number = acg.id_number
-  And realestcouncil.committee_short_desc = 'RealEstCouncil'
-Left Join members divsummit
-  On divsummit.id_number = acg.id_number
-  And divsummit.committee_short_desc = 'DivSummit'
-Left Join members womensummit
-  On womensummit.id_number = acg.id_number
-  And womensummit.committee_short_desc = 'WomenSummit'
-Left Join members corpgov
-  On corpgov.id_number = acg.id_number
-  And corpgov.committee_short_desc = 'CorpGov'
-Left Join members kfn
-  On kfn.id_number = acg.id_number
-  And kfn.committee_short_desc = 'KFN'
-Left Join members gab
-  On gab.id_number = acg.id_number
-  And gab.committee_short_desc = 'GAB'
-Left Join members healthcare
-  On healthcare.id_number = acg.id_number
-  And healthcare.committee_short_desc = 'Healthcare'
-Left Join members WomensLeadership
-  On WomensLeadership.id_number = acg.id_number
-  And WomensLeadership.committee_short_desc = 'WomensLeadership'
-Left Join members KAC
-  On KAC.id_number = acg.id_number
-  And KAC.committee_short_desc = 'KAC'
-Left Join members PHS
-  On KAC.id_number = acg.id_number
-  And KAC.committee_short_desc = 'PHS'
-Left Join members inclusion
-  On inclusion.id_number = acg.id_number
-  And inclusion.committee_short_desc = 'Inclusion'
-Left Join members private_equity
-  On private_equity.id_number = acg.id_number
-  And private_equity.committee_short_desc = 'PrivateEquity'
-Left Join members keba
-  On keba.id_number = acg.id_number
-  And keba.committee_short_desc = 'KEBA'
+Left Join all_committees_giving acg
+  On acg.id_number = ac.id_number
 ;
 
 /***********************************************
