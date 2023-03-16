685917711e0e17f10493d467f6b30529d6312cf9
Paul Hively <pbhively@gmail.com>
2023-03-16 17:44:08 -0500

    Test cases for ksm_pkg_committee rework
    - Add tests for accessor functions
    - #126

--- packages/test code/ksm_pkg_committee.sql
+++ packages/test code/ksm_pkg_committee.sql
@@ -2,57 +2,63 @@
 Select * From table(ksm_pkg_committee.tbl_committee_agg('U', 'GAB'))
 ;
 
+Select * From table(ksm_pkg_committee.tbl_committee_members(
+        ksm_pkg_committee.get_string_constant('committee_kac')
+    )
+)
+;
+
 -- Individual committee tests
-Select * From table(ksm_pkg_committee.tbl_committee_gab)
+Select * From table(ksm_pkg_tst.tbl_committee_gab)
 ;
 
-Select * From table(ksm_pkg_committee.tbl_committee_phs)
+Select * From table(ksm_pkg_tst.tbl_committee_phs)
 ;
 
-Select * From table(ksm_pkg_committee.tbl_committee_kac)
+Select * From table(ksm_pkg_tst.tbl_committee_kac)
 ;
 
-Select * From table(ksm_pkg_committee.tbl_committee_kfn)
+Select * From table(ksm_pkg_tst.tbl_committee_kfn)
 ;
 
-Select * From table(ksm_pkg_committee.tbl_committee_corpGov)
+Select * From table(ksm_pkg_tst.tbl_committee_corpGov)
 ;
 
-Select * From table(ksm_pkg_committee.tbl_committee_womenSummit)
+Select * From table(ksm_pkg_tst.tbl_committee_womenSummit)
 ;
 
-Select * From table(ksm_pkg_committee.tbl_committee_divSummit)
+Select * From table(ksm_pkg_tst.tbl_committee_divSummit)
 ;
 
-Select * From table(ksm_pkg_committee.tbl_committee_realEstCouncil)
+Select * From table(ksm_pkg_tst.tbl_committee_realEstCouncil)
 ;
 
-Select * From table(ksm_pkg_committee.tbl_committee_amp)
+Select * From table(ksm_pkg_tst.tbl_committee_amp)
 ;
 
-Select * From table(ksm_pkg_committee.tbl_committee_trustee)
+Select * From table(ksm_pkg_tst.tbl_committee_trustee)
 ;
 
-Select * From table(ksm_pkg_committee.tbl_committee_healthcare)
+Select * From table(ksm_pkg_tst.tbl_committee_healthcare)
 ;
 
-Select * From table(ksm_pkg_committee.tbl_committee_womensLeadership)
+Select * From table(ksm_pkg_tst.tbl_committee_womensLeadership)
 ;
 
-Select * From table(ksm_pkg_committee.tbl_committee_kalc)
+Select * From table(ksm_pkg_tst.tbl_committee_kalc)
 ;
 
-Select * From table(ksm_pkg_committee.tbl_committee_kic)
+Select * From table(ksm_pkg_tst.tbl_committee_kic)
 ;
 
-Select * From table(ksm_pkg_committee.tbl_committee_privateEquity)
+Select * From table(ksm_pkg_tst.tbl_committee_privateEquity)
 ;
 
-Select * From table(ksm_pkg_committee.tbl_committee_pe_asia)
+Select * From table(ksm_pkg_tst.tbl_committee_pe_asia)
 ;
 
-Select * From table(ksm_pkg_committee.tbl_committee_asia)
+Select * From table(ksm_pkg_tst.tbl_committee_asia)
 ;
 
-Select * From table(ksm_pkg_committee.tbl_committee_mbai)
+Select * From table(ksm_pkg_tst.tbl_committee_mbai)
 ;
