9d307ba0f047db9154843b27fc318daa21183fd2
Paul Hively <pbhively@gmail.com>
2023-03-16 15:33:55 -0500

    Ksm pkg committee test cases
    - Added .sql test cases for each ksm_pkg_committee table function
    - Formatting pass

---
+++ packages/test code/ksm_pkg_committee.sql
@@ -0,0 +1,58 @@
+-- Direct cursor test
+Select * From table(ksm_pkg_committee.tbl_committee_agg('U', 'GAB'))
+;
+
+-- Individual committee tests
+Select * From table(ksm_pkg_committee.tbl_committee_gab)
+;
+
+Select * From table(ksm_pkg_committee.tbl_committee_phs)
+;
+
+Select * From table(ksm_pkg_committee.tbl_committee_kac)
+;
+
+Select * From table(ksm_pkg_committee.tbl_committee_kfn)
+;
+
+Select * From table(ksm_pkg_committee.tbl_committee_corpGov)
+;
+
+Select * From table(ksm_pkg_committee.tbl_committee_womenSummit)
+;
+
+Select * From table(ksm_pkg_committee.tbl_committee_divSummit)
+;
+
+Select * From table(ksm_pkg_committee.tbl_committee_realEstCouncil)
+;
+
+Select * From table(ksm_pkg_committee.tbl_committee_amp)
+;
+
+Select * From table(ksm_pkg_committee.tbl_committee_trustee)
+;
+
+Select * From table(ksm_pkg_committee.tbl_committee_healthcare)
+;
+
+Select * From table(ksm_pkg_committee.tbl_committee_womensLeadership)
+;
+
+Select * From table(ksm_pkg_committee.tbl_committee_kalc)
+;
+
+Select * From table(ksm_pkg_committee.tbl_committee_kic)
+;
+
+Select * From table(ksm_pkg_committee.tbl_committee_privateEquity)
+;
+
+Select * From table(ksm_pkg_committee.tbl_committee_pe_asia)
+;
+
+Select * From table(ksm_pkg_committee.tbl_committee_asia)
+;
+
+Select * From table(ksm_pkg_committee.tbl_committee_mbai)
+;
