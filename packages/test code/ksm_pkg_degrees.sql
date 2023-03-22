560ee86a2a4b8271dd0367c33d6a4b1649ae388a
Paul Hively <pbhively@gmail.com>
2023-03-22 15:24:42 -0500

    Create ksm_pkg_degrees tests - Closes #136

---
+++ packages/test code/ksm_pkg_degrees.sql
@@ -0,0 +1,23 @@
+---------------------------
+-- ksm_pkg_degrees tests
+---------------------------
+
+Select *
+From table(ksm_pkg_degrees.tbl_entity_degrees_concat_ksm)
+;
+
+Select
+    deg.id_number
+    , deg.degrees_concat
+    , ksm_pkg_degrees.get_entity_degrees_concat_fast(id_number)
+        As deg_conc_from_func
+From table(ksm_pkg_degrees.tbl_entity_degrees_concat_ksm) deg
+;
+
+---------------------------
+-- ksm_pkg tests
+---------------------------
+
+Select *
+From table(ksm_pkg_tst.tbl_entity_degrees_concat_ksm)
+;
