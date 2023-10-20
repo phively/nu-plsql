2fdbf03514afb0a400fbcc837ee53bbacd59878b
Paul Hively <pbhively@gmail.com>
2023-10-20 16:30:33 -0500

    Cash view: proposal manager attribution - Linked proposal manager to gifts and pledges with a proposal

--- views/v_ksm_giving_cash.sql
+++ views/v_ksm_giving_cash.sql
@@ -47,6 +47,13 @@ hhf As (
     alloc_school = 'KM'
 )
 
+, funded_proposal_credit As (
+  Select
+    proposal_id
+    , assignment_id_number
+  From table(rpt_pbh634.metrics_pkg.tbl_funded_count(ask_amt => 0.01, funded_count => 0.01))
+)
+
 , attr_cash As (
   Select
     gt.tx_number
@@ -65,10 +72,20 @@ hhf As (
         Else allocs.cash_category
         End 
       As cash_category
-    , gt.proposal_id
+    , gt.pledge_number
+    -- Use proposal ID directly if available; if a pledge payment type look for pledge proposal ID
+    , Case
+        When gt.proposal_id Is Not Null
+          Then gt.proposal_id
+        When gt.pledge_number Is Not Null
+          Then primary_pledge.proposal_id
+        End
+      As proposal_id
   From v_ksm_giving_trans gt
   Inner Join allocs
     On allocs.allocation_code = gt.allocation_code
+  Left Join primary_pledge
+    On primary_pledge.prim_pledge_number = gt.pledge_number
   Where gt.legal_amount > 0
     And gt.tx_gypm_ind <> 'P'
     And gt.fiscal_year = 2023
@@ -76,7 +93,9 @@ hhf As (
 
 Select
   attr_cash.*
-  , NULL As proposal_mgr
+  , assignment_id_number As proposal_mgr
   , NULL As assigned_pm
   , NULL As assigned_lgo
 From attr_cash
+Left Join funded_proposal_credit fpc
+  On fpc.proposal_id = attr_cash.proposal_id
