1eba27f038b853050f3f4d0f8e723c30c6b71f52
Paul Hively <pbhively@gmail.com>
2023-12-21 15:06:04 -0600

    Added ytd columns to vt_cash_hierarchy - New columns for all cash and retention fields, plus checks - Closes #183

--- views/vt_cash_hierarchy.sql
+++ views/vt_cash_hierarchy.sql
@@ -28,6 +28,8 @@ hhf As (
     , attr_cash.managed_grp
     , sum(attr_cash.legal_amount)
       As sum_legal_amount
+    , sum(Case When attr_cash.fytd_ind = 'Y' Then attr_cash.legal_amount Else 0 End)
+      As sum_legal_amount_ytd
     , count(household_id) Over(Partition By cash_category, household_id, fiscal_year)
       As n_managed_in_group
   From attr_cash
@@ -50,10 +52,28 @@ hhf As (
       , fiscal_year
       , managed_grp
       , sum_legal_amount
-    , n_managed_in_group
+      , n_managed_in_group
   From grouped_cash
   ) Pivot (
     sum(sum_legal_amount)
+    For managed_grp In ('Unmanaged', 'MGO', 'LGO', 'KSM', 'NU')
+  )
+)
+
+, pivot_cash_ytd As (
+  -- Pivot cash category/household/fiscal year by managed group, for later use with board dues
+  Select *
+  From (
+    Select
+      cash_category
+      , household_id
+      , fiscal_year
+      , managed_grp
+      , sum_legal_amount_ytd
+      , n_managed_in_group
+  From grouped_cash
+  ) Pivot (
+    sum(sum_legal_amount_ytd)
     For managed_grp In ('Unmanaged', 'MGO', 'LGO', 'KSM', 'NU')
   )
 )
@@ -73,6 +93,21 @@ hhf As (
   )
 )
 
+, pivot_retention_ytd As (
+  -- Pivot sum(legal_amount) by fiscal year, to compute retained status
+  Select *
+  From (
+    Select
+      household_id
+      , fiscal_year
+      , sum_legal_amount_ytd
+    From grouped_cash
+  ) Pivot (
+    sum(sum_legal_amount_ytd)
+    For fiscal_year In (2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030)
+  )
+)
+
 , cash_retention As (
     Select 
       pivot_retention.household_id
@@ -88,9 +123,33 @@ hhf As (
     From pivot_retention
 )
 
+, cash_retention_ytd As (
+    Select 
+      pivot_retention_ytd.household_id
+      , Case When "2022" > 0 And "2021" > 0 Then 'Y' End As "2022"
+      , Case When "2023" > 0 And "2022" > 0 Then 'Y' End As "2023"
+      , Case When "2024" > 0 And "2023" > 0 Then 'Y' End As "2024"
+      , Case When "2025" > 0 And "2024" > 0 Then 'Y' End As "2025"
+      , Case When "2026" > 0 And "2025" > 0 Then 'Y' End As "2026"
+      , Case When "2027" > 0 And "2026" > 0 Then 'Y' End As "2027"
+      , Case When "2028" > 0 And "2027" > 0 Then 'Y' End As "2028"
+      , Case When "2029" > 0 And "2028" > 0 Then 'Y' End As "2029"
+      , Case When "2030" > 0 And "2029" > 0 Then 'Y' End As "2030"
+    From pivot_retention_ytd
+)
+
 , retention_flag As (
   Select Distinct *
   From cash_retention
+  Unpivot (
+    retained
+    For retained_year In ("2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030")
+  )
+)
+
+, retention_flag_ytd As (
+  Select Distinct *
+  From cash_retention_ytd
   Unpivot (
     retained
     For retained_year In ("2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030")
@@ -433,28 +492,245 @@ hhf As (
 )
 
 -- Unpivot final results
+, finalnonytd As (
+  Select
+    unpiv.*
+    -- Use retained_cfy to use CURRENT FY retention with CFY as the denominator
+    -- e.g. count(distinct household_id) where fiscal_year = cal.cfy group by retained_cfy
+    , retain_cfy.retained As retained_cfy
+    , retain_cfy.retained_year As retained_cfy_year
+    -- Use retained_nfy to compute NEXT FY retention with CFY as denominator
+    -- e.g. count(distinct household_id) where fiscal_year = cal.cfy group by retained_nfy
+    , retain_nfy.retained As retained_nfy
+    , retain_nfy.retained_year As retained_nfy_year
+  From (
+    Select *
+    From prefinal_data
+    Unpivot (
+      legal_amount
+      For managed_grp In ("Boards", "Unmanaged", "KSM", "NU", "MGO", "LGO")
+    )
+  ) unpiv
+  Left Join retention_flag retain_cfy
+    On retain_cfy.household_id = unpiv.household_id
+    And retain_cfy.retained_year = unpiv.fiscal_year
+  Left Join retention_flag retain_nfy
+    On retain_nfy.household_id = unpiv.household_id
+    And retain_nfy.retained_year - 1 = unpiv.fiscal_year
+)
+
+-- Build ytd results
+, merged_data_ytd As (
+  Select
+    gc.cash_category
+    , gc.household_id
+    , gc.fiscal_year
+/*    , gc.n_managed_in_group
+    , boards_hh.gab
+    , boards_hh.ebfa
+    , boards_hh.amp
+    , boards_hh.re
+    , boards_hh.health
+    , boards_hh.peac
+    , boards_hh.kac
+    , boards_hh.kwlc
+*/    -- Board dues should come out of Expendable funds
+    , Case
+        When gc.cash_category = 'Expendable'
+          Then boards_hh.total_dues_hh
+        End
+      As total_dues_hh
+    -- Board dues: first take from unmanaged, then KSM, then NU, then MGO, then LGO
+    -- For expendable, board_amt is at least sum_legal_amount up to total_dues_hh
+    , Case
+        When cash_category = 'Expendable'
+          And total_dues_hh Is Not Null
+          Then
+        -- Check if Unmanaged >= total_dues_hh
+        Case
+          When nvl("'Unmanaged'", 0) >= boards_hh.total_dues_hh
+            Then 'U'
+        -- Check if Unmanaged + KSM >= total_dues_hh
+          When nvl("'Unmanaged'", 0) + nvl("'KSM'", 0) >= boards_hh.total_dues_hh
+            Then 'UK'
+        -- Check if Unmanaged + KSM + NU >= total_dues_hh
+          When nvl("'Unmanaged'", 0) + nvl("'KSM'", 0) + nvl("'NU'", 0) >= boards_hh.total_dues_hh
+            Then 'UKN'
+        -- Check if Unmanaged + KSM + NU + MGO >= total_dues_hh
+          When nvl("'Unmanaged'", 0) + nvl("'KSM'", 0) + nvl("'NU'", 0) + nvl("'MGO'", 0) >= boards_hh.total_dues_hh
+            Then 'UKNM'
+        -- Fallback: take from everything
+          Else 'UKNML'
+          End
+        End
+      As boards_cash_source
+    , nvl(gc."'Unmanaged'", 0)
+      As U
+    , nvl(gc."'MGO'", 0)
+      As M
+    , nvl(gc."'LGO'", 0)
+      As L
+    , nvl(gc."'KSM'", 0)
+      As K
+    , nvl(gc."'NU'", 0)
+      As N
+  From pivot_cash_ytd gc
+  Left Join boards_hh
+    On boards_hh.household_id = gc.household_id
+    And boards_hh.fiscal_year = gc.fiscal_year
+)
+
+, prefinal_data_ytd As (
+  Select
+    merged_data_ytd.cash_category
+    , merged_data_ytd.household_id
+    , merged_data_ytd.fiscal_year
+    , merged_data_ytd.total_dues_hh
+    , merged_data_ytd.boards_cash_source
+    , U
+    , K
+    , N
+    , M
+    , L
+    -- Compute board dues based on the boards_cash_source string
+    -- Use least() function to ensure amount is at most total_dues_hh
+    , Case
+        When boards_cash_source = 'U'
+          Then nullif(least(U, total_dues_hh), 0)
+        When boards_cash_source = 'UK'
+          Then nullif(least(U + K, total_dues_hh), 0)
+        When boards_cash_source = 'UKN'
+          Then nullif(least(U + K + N, total_dues_hh), 0)            
+        When boards_cash_source = 'UKNM'
+          Then nullif(least(U + K + N + M, total_dues_hh), 0)
+        When boards_cash_source = 'UKNML'
+          Then nullif(least(U + K + N + M + L, total_dues_hh), 0)
+        End
+      As "Boards"
+    -- Zero (null) out the corresponding column if boards_cash_source found that amount is needed for dues
+    -- Use greatest() function to ensure remaining amount is at least 0
+    , Case
+        When boards_cash_source = 'U'
+          Then nullif(greatest(U - total_dues_hh, 0), 0)
+        When regexp_like(boards_cash_source, 'K|N|M|L')
+          Then Null
+        Else nullif(U, 0)
+        End
+      As "Unmanaged"
+    , Case
+        When boards_cash_source Like '%K'
+          Then nullif(greatest(U + K - total_dues_hh, 0), 0)
+        When regexp_like(boards_cash_source, 'N|M|L')
+          Then Null
+        Else nullif(K, 0)
+        End
+      As "KSM"
+    , Case
+        When boards_cash_source Like '%N'
+          Then nullif(greatest(U + K + N - total_dues_hh, 0), 0)
+        When regexp_like(boards_cash_source, 'M|L')
+          Then Null
+        Else nullif(N, 0)
+        End
+      As "NU"
+    , Case
+        When boards_cash_source Like '%M'
+          Then nullif(greatest(U + K + N + M - total_dues_hh, 0), 0)
+        When regexp_like(boards_cash_source, 'L')
+          Then Null
+        Else nullif(M, 0)
+        End
+      As "MGO"
+    , Case
+        When boards_cash_source Like '%L'
+          Then nullif(greatest(U + K + N + M + L - total_dues_hh, 0), 0)
+        Else nullif(L, 0)
+        End
+      As "LGO"
+  From merged_data_ytd
+)
+
+-- Unpivot ytd results
+, finalytd As (
+  Select
+    unpiv.*
+    -- Use retained_cfy to use CURRENT FY retention with CFY as the denominator
+    -- e.g. count(distinct household_id) where fiscal_year = cal.cfy group by retained_cfy
+    , retain_cfy.retained As retained_cfy
+    , retain_cfy.retained_year As retained_cfy_year
+    -- Use retained_nfy to compute NEXT FY retention with CFY as denominator
+    -- e.g. count(distinct household_id) where fiscal_year = cal.cfy group by retained_nfy
+    , retain_nfy.retained As retained_nfy
+    , retain_nfy.retained_year As retained_nfy_year
+  From (
+    Select *
+    From prefinal_data_ytd
+    Unpivot (
+      legal_amount
+      For managed_grp In ("Boards", "Unmanaged", "KSM", "NU", "MGO", "LGO")
+    )
+  ) unpiv
+  Left Join retention_flag_ytd retain_cfy
+    On retain_cfy.household_id = unpiv.household_id
+    And retain_cfy.retained_year = unpiv.fiscal_year
+  Left Join retention_flag_ytd retain_nfy
+    On retain_nfy.household_id = unpiv.household_id
+    And retain_nfy.retained_year - 1 = unpiv.fiscal_year
+)
+
 Select
-  unpiv.*
-  -- Use retained_cfy to use CURRENT FY retention with CFY as the denominator
-  -- e.g. count(distinct household_id) where fiscal_year = cal.cfy group by retained_cfy
-  , retain_cfy.retained As retained_cfy
-  , retain_cfy.retained_year As retained_cfy_year
-  -- Use retained_nfy to compute NEXT FY retention with CFY as denominator
-  -- e.g. count(distinct household_id) where fiscal_year = cal.cfy group by retained_nfy
-  , retain_nfy.retained As retained_nfy
-  , retain_nfy.retained_year As retained_nfy_year
-From (
-  Select *
-  From prefinal_data
-  Unpivot (
-    legal_amount
-    For managed_grp In ("Boards", "Unmanaged", "KSM", "NU", "MGO", "LGO")
-  )
-) unpiv
-Left Join retention_flag retain_cfy
-  On retain_cfy.household_id = unpiv.household_id
-  And retain_cfy.retained_year = unpiv.fiscal_year
-Left Join retention_flag retain_nfy
-  On retain_nfy.household_id = unpiv.household_id
-  And retain_nfy.retained_year - 1 = unpiv.fiscal_year
+  f.cash_category
+  , f.household_id
+  , f.fiscal_year
+  , f.n_managed_in_group
+  , f.gab
+  , f.ebfa
+  , f.amp
+  , f.re
+  , f.health
+  , f.peac
+  , f.kac
+  , f.kwlc
+  , f.total_dues_hh
+  , f.boards_cash_source
+  , f.u
+  , f.k
+  , f.n
+  , f.m
+  , f.l
+  , f.managed_grp
+  , f.legal_amount
+  , f.retained_cfy
+  , f.retained_cfy_year
+  , f.retained_nfy
+  , f.retained_nfy_year
+  -- YTD columns
+  , fy.boards_cash_source
+    As ytd_boards_cash_source
+  , fy.u
+    As ytd_u
+  , fy.k
+    As ytd_k
+  , fy.n
+    As ytd_n
+  , fy.m
+    As ytd_m
+  , fy.l
+    As ytd_l
+  , fy.legal_amount
+    As ytd_legal_amount
+  , fy.retained_cfy
+    As ytd_retained_cfy
+  , fy.retained_cfy_year
+    As ytd_retained_cfy_year
+  , fy.retained_nfy
+    As ytd_retained_nfy
+  , fy.retained_nfy_year
+    As ytd_retained_nfy_year
+From finalnonytd f
+Left Join finalytd fy
+  On fy.household_id = f.household_id
+  And fy.fiscal_year = f.fiscal_year
+  And fy.cash_category = f.cash_category
+  And fy.managed_grp = f.managed_grp
 ;
