4fc8ad74c21ba62910ad750fce62d2d617028079
Paul Hively <pbhively@gmail.com>
2024-01-11 16:21:52 -0600

    Alumni survey sample FY24 update - Added first name salutations - Flagged other survey populations - Added master exclusion flag - Added decade

--- sql code/Survey population.sql
+++ sql code/Survey population.sql
@@ -15,21 +15,59 @@ pref_email As (
     And email.preferred_ind = 'Y'
 )
 
+, salutation As (
+  Select
+    sal.id_number
+    , sal.p_dean_salut
+    , sal.joint_dean_salut
+  From rpt_zrc8929.v_dean_salutation sal
+)
+
 -- Look up other FY24 survey recipients for exclusions
+, imcpanel As (
+  Select id_number
+  From tbl_imc_survey_panel_2023
+)
+
+, boardfellows As (
+  Select
+    id_number
+    , segment
+  From tbl_board_fellows_survey_2024
+)
 
 Select
   deg.id_number
+  , sal.p_dean_salut
   , deg.report_name
   , deg.record_status_code
   , deg.first_masters_year
+  , trunc(deg.first_masters_year/10) * 10
+    As masters_decade
   , deg.program_group
   , deg.program
   , entity.gender_code
+  , Case
+      When sh.gab Is Not Null
+        Or sh.ebfa Is Not Null
+        Or sh.trustee Is Not Null
+        Or sh.no_contact Is Not Null
+        Or sh.no_email_ind Is Not Null
+        Or sh.no_survey_ind Is Not Null
+        Or imcpanel.id_number Is Not Null
+        Or boardfellows.id_number Is Not Null
+        Then 'Y'
+      End
+    As any_exclusion
   , sh.gab
   , sh.ebfa
   , sh.trustee
   , sh.no_email_ind
   , sh.no_survey_ind
+  , Case When imcpanel.id_number Is Not Null Then 'Y' End
+    As imc_panel_survey
+  , boardfellows.segment
+    As board_fellows_survey_segment
   , sh.special_handling_concat
   , sh.mailing_list_concat
   , pref_email.pref_email
@@ -41,11 +79,17 @@ Left Join pref_email
   On pref_email.id_number = deg.id_number
 Left Join v_entity_special_handling sh
   On sh.id_number = deg.id_number
+Left Join imcpanel
+  On imcpanel.id_number = deg.id_number
+Left Join boardfellows
+  On boardfellows.id_number = deg.id_number
+Left Join salutation sal
+  On sal.id_number = deg.id_number
 Where
   -- Active alumni only
   deg.record_status_code = 'A'
   -- MBA only, dropping PHD for 2024
   And deg.first_masters_year Is Not Null
   -- Exclude international alumni
-  And program Not In ('EMP-ISR', 'EMP-CAN', 'EMP-HK', 'EMP-GER', 'EMP-CHI')
+  And deg.program Not In ('EMP-ISR', 'EMP-CAN', 'EMP-HK', 'EMP-GER', 'EMP-CHI')
   
