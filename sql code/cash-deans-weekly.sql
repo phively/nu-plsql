c9cbfe82760047c13b4f158bc30fab4b8a568339
Paul Hively <pbhively@gmail.com>
2024-02-15 12:20:53 -0600

    Deans Weekly pfy2 updates - Added PFY2 total to cash code - Cash categories are now sorted - New FRP person/org file; pulls 3 years at once

--- sql code/cash-deans-weekly.sql
+++ sql code/cash-deans-weekly.sql
@@ -4,17 +4,19 @@ With
 -- Can tweak fiscal years and which day to use as previous day below
 -- For example, for EOFY 2023 use:
 /*
-    2022 As prev_fy
+    2021 As prev_fy
     , 2023 As curr_fy
     , to_date('20230831', 'yyyymmdd') As prev_day
 */
+
 cal As (
   Select
-    curr_fy - 1 As prev_fy
+    curr_fy - 2 As prev_fy
     , curr_fy - 0 As curr_fy
     , yesterday As prev_day
   From rpt_pbh634.v_current_calendar
 )
+
 , ytd_dts As (
   Select
     to_date('09/01/' || (cal.prev_fy - 1), 'mm/dd/yyyy') + rownum - 1 As dt
@@ -50,15 +52,21 @@ cal As (
     , cal.prev_fy
     , cal.prev_day
     , Case
-      When gft.cash_category Not In ('KEC', 'Expendable', 'Hub Campaign Cash')
-        Then 'Other Cash'
-      Else gft.cash_category
-      End
+        When gft.cash_category Not In ('KEC', 'Expendable', 'Hub Campaign Cash')
+          Then 'Z-Other Cash'
+        When gft.cash_category = 'Expendable'
+          Then 'A-Expendable'
+        When gft.cash_category = 'KEC'
+          Then 'B-KEC'
+        When gft.cash_category = 'Hub Campaign Cash'
+          Then 'C-Hub Campaign Cash'
+        Else gft.cash_category
+        End
       As cash_type
   From rpt_pbh634.v_ksm_giving_cash gft
   Cross Join cal
-  Inner Join ytd_dts On trunc(ytd_dts.dt) = trunc(gft.date_of_record)
-  Where fiscal_year In (cal.curr_fy, cal.prev_fy)
+    Inner Join ytd_dts On trunc(ytd_dts.dt) = trunc(gft.date_of_record)
+  Where fiscal_year Between cal.prev_fy And cal.curr_fy
     And ytd_dts.ytd_ind = 'Y'
   -- Manual exceptions
   Union
@@ -73,19 +81,19 @@ cal As (
     , g.fiscal_year
     , g.date_of_record
     , g.legal_amount
-    , 'Expendable'
+    , 'A-Expendable'
       As cash_category
     , cal.curr_fy
     , cal.prev_fy
     , cal.prev_day
-    , 'Expendable'
+    , 'A-Expendable'
       As cash_type
   From nu_gft_trp_gifttrans g
   Inner Join entity
     On entity.id_number = g.id_number
   Cross Join cal
   Inner Join ytd_dts On trunc(ytd_dts.dt) = trunc(g.date_of_record)
-  Where g.fiscal_year In (cal.curr_fy, cal.prev_fy)
+  Where g.fiscal_year Between cal.prev_fy And cal.curr_fy
     And ytd_dts.ytd_ind = 'Y'
     And (
       g.tx_number = 3012462 And g.tx_sequence = 2
@@ -95,8 +103,9 @@ cal As (
 -- Main query
 Select
   cash_type
-  , sum(Case When fiscal_year = curr_fy Then legal_amount Else 0 End) As cfy_ytd_cash
-  , sum(Case When fiscal_year = prev_fy Then legal_amount Else 0 End) As pfy_ytd_cash
+  , sum(Case When fiscal_year = curr_fy - 0 Then legal_amount Else 0 End) As cfy_ytd_cash
+  , sum(Case When fiscal_year = curr_fy - 1 Then legal_amount Else 0 End) As pfy1_ytd_cash
+  , sum(Case When fiscal_year = curr_fy - 2 Then legal_amount Else 0 End) As pfy2_ytd_cash
   , prev_day As as_of
 From cash
 Group By cash_type, prev_day
