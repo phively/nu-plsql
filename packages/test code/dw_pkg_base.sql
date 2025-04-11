55c21bc27e82db36f7d498145fd3999856e4e8b5
Paul Hively <pbhively@gmail.com>
2025-04-11 16:46:23 -0500

    Test cases for dw_pkg_base - Added simple select statements to verify table functions return results

---
+++ packages/test code/dw_pkg_base.sql
@@ -0,0 +1,16 @@
+---------------------------
+-- dw_pkg_base tests
+---------------------------
+
+-- Verify that table functions work
+Select *
+From table(dw_pkg_base.tbl_constituent)
+;
+
+Select *
+From table(dw_pkg_base.tbl_organization)
+;
+
+Select *
+From table(dw_pkg_base.tbl_designation)
+;
