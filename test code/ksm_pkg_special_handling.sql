20314155427b0a6360f0f4598045e89b2bbe1521
Paul Hively <pbhively@gmail.com>
2025-05-28 11:20:27 -0500

    Special handling rework, initial tests - Materialized view runs - Copied test file from old CATx code; add remaining tests - Updated data hierarchy - For #207

---
+++ test code/ksm_pkg_special_handling.sql
@@ -0,0 +1,50 @@
+---------------------------
+-- ksm_pkg_special_handling tests
+---------------------------
+
+-- Totals
+Select count(*)
+From table(ksm_pkg_special_handling.tbl_special_handling)
+;
+
+---------------------------
+-- mv tests
+---------------------------
+/*
+-- No Contact
+With
+nc As (
+  Select *
+  From table(ksm_pkg_special_handling.tbl_special_handling_concat) shc
+  Where shc.spec_hnd_codes Like '%NC%'
+)
+Select *
+From nc
+Where rownum <= 10
+;
+
+-- No Solicit
+With
+ns As (
+  Select *
+  From table(ksm_pkg_special_handling.tbl_special_handling_concat) shc
+  Where shc.spec_hnd_codes Not Like '%NC%'
+    And shc.spec_hnd_codes Like '%DNS%'
+)
+Select *
+From ns
+Where rownum <= 10
+;
+
+-- Trustee check
+With
+trustee As (
+  Select *
+  From table(ksm_pkg_special_handling.tbl_special_handling_concat) shc
+  Where shc.trustee Is Not Null
+)
+Select *
+From trustee
+Where rownum <= 10
+;
+*/
