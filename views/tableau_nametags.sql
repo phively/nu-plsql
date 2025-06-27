fae805e018722ec11efd3801e9d72e4d4ef30022
Sergio Socite <sergio.socite@gmail.com>
2025-06-27 10:53:23 -0500

    Update tableau_nametags.sql

--- views/tableau_nametags.sql
+++ views/tableau_nametags.sql
@@ -6,7 +6,10 @@ DEGREE_YEAR ,
 DEGREE_CODE ,
 DEGREE_NAME ,
 DEGREE_PROGRAM  
-From table(dw_pkg_base.tbl_degrees)),
+From table(dw_pkg_base.tbl_degrees)
+--- Northwestern Related Degrees Only
+where DEGREE_ORGANIZATION_NAME = 'Northwestern University'
+),
 
 e as (select
 k.CONSTITUENT_DONOR_ID,
@@ -46,7 +49,7 @@ from e),
 degrees_group_by_year as (
 select dc.CONSTITUENT_DONOR_ID,
 dc.degree_year,
-dc.degree_code,
+dc.ksm_degree,
 '''' || substr(degree_year, -2) -- Last two digits of Year on Nametag
 As year_abbr,
 --- Listagg multiple years - Order by degree asc
@@ -54,26 +57,28 @@ Listagg(trim(dc.degree_string), ', ') Within Group (Order By degree_year Asc)
 as degree_strings,
 Listagg(trim(dc.degree_level), ', ') Within Group (Order By degree_year Asc)
 as degree_levels
-
 from degrees_clean dc
 group by dc.CONSTITUENT_DONOR_ID,
 dc.degree_year,
-dc.degree_code),
+dc.degree_code,
+dc.ksm_degree),
 
 --- Final concat
 
 degrees_concat As (Select degrees_group_by_year.CONSTITUENT_DONOR_ID,
+degrees_group_by_year.ksm_degree,
 Listagg(trim(year_abbr || ' ' || degree_strings), ', '
 ) Within Group (Order By degree_year Asc, degree_strings Asc) As nu_degrees_string,
 Listagg(trim(year_abbr || ' ' || degrees_group_by_year.degree_levels), ', '
 ) Within Group (Order By degree_year Asc, degree_strings Asc) As degree_levels
-
 From degrees_group_by_year
-Group By CONSTITUENT_DONOR_ID)
+Group By CONSTITUENT_DONOR_ID,
+degrees_group_by_year.ksm_degree
+)
 
 Select distinct k.CONSTITUENT_DONOR_ID,
 k.CONSTITUENT_NAME,
-case when k.DEGREE_SCHOOL_NAME like '%Kellogg%'   then 'Y'  end as KSM_degree,
+dc.ksm_degree,
 dc.degree_levels,
 dc.nu_degrees_string
 from k
