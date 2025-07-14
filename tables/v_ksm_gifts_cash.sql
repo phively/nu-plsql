f2c509133d9f8d038749c5c0ec1b020c736eabec
Paul Hively <pbhively@gmail.com>
2025-07-14 13:03:36 -0500

    v_ksm_gifts_cash manual entries logic - Enabled UNION of manual transactions to v_ksm_gifts_cash. Requires custom entry of these datapoints: --- tx_id, copy_tx_id_from, designation_record_id, gypm_ind, credit_amount, credit_date - Updated test script

--- tables/v_ksm_gifts_cash.sql
+++ tables/v_ksm_gifts_cash.sql
@@ -8,7 +8,7 @@ Create Or Replace View v_ksm_gifts_cash As
 With
 
 -- Cash counting exceptions
--- Add additional entries as a new Union All section
+-- Add override entries as a new Union All section
 override As (
   Select
     NULL As tx_id
@@ -21,6 +21,156 @@ override As (
   Union All
   Select 'T2536547', 200000.00 * 2
   From DUAL
+)
+-- Add exception entries as a new Union All section
+, manual_gift_exceptions As (
+  Select
+    NULL As tx_id
+    , NULL As copy_tx_id_from -- Copy misc gift info from this tx_id
+    , NULL As designation_record_id
+    , NULL As gypm_ind
+    , NULL As credit_amount
+    , NULL As credit_date
+  From DUAL
+  -- IC
+  Union All
+  Select
+    'IC001'
+    , 'PN2297936'
+    , 'N3037925'
+    , 'Y'
+    , 555000.00
+    , to_date('20250630', 'yyyymmdd')
+  From DUAL
+)
+
+, kt_custom As (
+  Select
+    kt.credited_donor_audit
+    , kt.opportunity_donor_id
+    , kt.opportunity_donor_name
+    , kt.credited_donor_id
+    , kt.tribute_type
+    , kt.tx_id
+    , kt.opportunity_record_id
+    , kt.anonymous_type
+    , kt.legacy_receipt_number
+    , kt.opportunity_stage
+    , kt.opportunity_record_type
+    , kt.opportunity_type
+    , kt.adjusted_opportunity_ind
+    , kt.source_type
+    , kt.source_type_detail
+    , kt.gypm_ind
+    , kt.hard_and_soft_credit_salesforce_id
+    , kt.credit_receipt_number
+    , kt.matched_gift_record_id
+    , kt.pledge_record_id
+    , kt.payment_schedule
+    , kt.linked_proposal_record_id
+    , kt.historical_pm_name
+    , kt.historical_pm_unit
+    , kt.historical_prm_name
+    , kt.historical_prm_unit
+    , kt.historical_lagm_name
+    , kt.historical_lagm_unit
+    , kt.historical_credit_user_id
+    , kt.historical_credit_name
+    , kt.historical_credit_assignment_type
+    , kt.historical_credit_unit
+    , kt.historical_credit_active_flag
+    , kt.designation_record_id
+    , kt.designation_status
+    , kt.legacy_allocation_code
+    , kt.designation_name
+    , kt.fin_fund_id
+    , kt.fin_department_id
+    , kt.fin_project_id
+    , kt.fin_activity
+    , kt.ksm_af_flag
+    , kt.ksm_cru_flag
+    , kt.cash_category
+    , kt.full_circle_campaign_priority
+    , kt.credit_date
+    , kt.fiscal_year
+    , kt.entry_date
+    , kt.credit_type
+    , kt.credit_amount
+    , kt.hard_credit_amount
+    , kt.recognition_credit
+    , kt.hh_credit
+    , kt.hh_recognition_credit
+    , kt.hh_credited_donors
+    , kt.tender_type
+    , kt.max_etl_update_date
+    , kt.mv_last_refresh
+  From mv_ksm_transactions kt
+  Union
+  Select
+    kt.credited_donor_audit
+    , kt.opportunity_donor_id
+    , kt.opportunity_donor_name
+    , kt.credited_donor_id
+    , NULL
+    , mge.tx_id
+    , NULL
+    , kt.anonymous_type
+    , NULL
+    , NULL
+    , 'Manual Add' As opportunity_record_type
+    , 'Manual Add' As opportunity_type
+    , NULL
+    , 'Internal Transfer' As source_type
+    , 'Internal Transfer' As source_type_detail
+    , mge.gypm_ind
+    , NULL
+    , NULL
+    , NULL
+    , NULL
+    , NULL
+    , NULL
+    , kt.historical_pm_name
+    , kt.historical_pm_unit
+    , kt.historical_prm_name
+    , kt.historical_prm_unit
+    , kt.historical_lagm_name
+    , kt.historical_lagm_unit
+    , kt.historical_credit_user_id
+    , kt.historical_credit_name
+    , kt.historical_credit_assignment_type
+    , kt.historical_credit_unit
+    , kt.historical_credit_active_flag
+    , mge.designation_record_id
+    , kt.designation_status
+    , NULL
+    , kdes.designation_name
+    , kdes.fin_fund_id
+    , kdes.fin_department_id
+    , kdes.fin_project_id
+    , kdes.fin_activity_id
+    , kdes.ksm_af_flag
+    , kdes.ksm_cru_flag
+    , kdes.cash_category
+    , kdes.full_circle_campaign_priority
+    , mge.credit_date
+    , ksm_pkg_calendar.get_fiscal_year(mge.credit_date) As fiscal_year
+    , NULL
+    , 'Hard' As credit_type
+    , mge.credit_amount As credit_amount
+    , mge.credit_amount As hard_credit_amount
+    , mge.credit_amount As recognition_credit
+    , mge.credit_amount As hh_credit
+    , mge.credit_amount As hh_recognition_credit
+    , 1 As hh_credited_donors
+    , 'Internal Transfer' As tender_type
+    , kt.max_etl_update_date
+    , kt.mv_last_refresh
+  From manual_gift_exceptions mge
+  Inner Join mv_ksm_transactions kt
+    On kt.tx_id = mge.copy_tx_id_from
+    And kt.credit_type = 'Hard'
+  Inner Join mv_ksm_designation kdes
+    On kdes.designation_record_id = mge.designation_record_id
 )
 
 , ksm_mgrs As (
@@ -198,7 +348,7 @@ Select
   , kt.max_etl_update_date
   , kt.mv_last_refresh
   , cal.today
-From mv_ksm_transactions kt
+From kt_custom kt
 Cross Join v_current_calendar cal
 Inner Join mv_entity mve
   On mve.donor_id = kt.credited_donor_id
