39619c9b5fbb28ad69188a4ac0f7e1c373b654ea
Sergio Socite <sergio.socite@gmail.com>
2026-01-13 16:19:52 -0600

    Update Employment Helper.sql

--- sql code/Employment Helper.sql
+++ sql code/Employment Helper.sql
@@ -1,21 +1,8 @@
 --- pulling linkedin
 
-with a as (select
-stg_alumni.ucinn_ascendv2__social_media__c.ucinn_ascendv2__contact__c,
-max (stg_alumni.ucinn_ascendv2__social_media__c.ucinn_ascendv2__url__c) keep (dense_rank first order by stg_alumni.ucinn_ascendv2__social_media__c.lastmodifieddate) as Linkedin_address
-from stg_alumni.ucinn_ascendv2__social_media__c
-where stg_alumni.ucinn_ascendv2__social_media__c.ap_status__c like '%Current%'
-and stg_alumni.ucinn_ascendv2__social_media__c.ucinn_ascendv2__platform__c = 'LinkedIn'
-group BY stg_alumni.ucinn_ascendv2__social_media__c.ucinn_ascendv2__contact__c
+with l as (select i.donor_id, i.linkedin_url
+from mv_entity_contact_info i
 ),
-
--- max(linkedin_url) keep dense_rank first ...
---- Using Keep Dense Rank
-
-l as (select distinct c.ucinn_ascendv2__donor_id__c,
-a.linkedin_address
-from stg_alumni.contact c
-inner join a on c.id = a.ucinn_ascendv2__contact__c),
 
 --- Employment - primary
 --- Also use keep dense rank function to pull most recent employee start date
@@ -33,61 +20,58 @@ c.ucinn_ascendv2__end_date__c as end_date,
 c.etl_update_date,
 c.etl_create_date,
 c.ucinn_ascendv2__notes__c as notes
-from stg_alumni.ucinn_ascendv2__Affiliation__c c),
-
---- C-Suite Flag
---- Employment with Senior Titles
-
-csuite as (select
-employ.id_number
-from employ
-where   ((job_title like '%Vice President%'
-or job_title like '%VP%'
-or job_title like '%Owner%'
-or job_title like '%Founder%'
-or job_title like '%Managing Director%'
-or job_title like '%Executive%'
-or job_title like '%Partner%'
-or job_title like '%President%'
-or job_title like '%Principal%'
-or job_title like '%Head%'
----or job_title like '%Senior%'
-or job_title like '%Chief%'
-or job_title like '%Board%'
----- Check Abbreviations too 
-or job_title like '%CEO%'
---- Chief Finance Officer
-or job_title like '%CFO%'
---- Chief Marketing Officer
-or job_title like '%CMO%'
---- Chief Information Officer
-or job_title like '%CIO%'
---- Chiefer Operating Office
-or job_title like '%COO%'
---- Chief Tech Officer
-or job_title like '%CTO%'
---- Chief Compliance officer
-or job_title like '%CCO%'
-
-
-
-)
-
---- take out assistants/associates/advisors, not actual senior titles
-
-and (job_title not like '%Assistant%'
-and job_title not like '%Asst%'
-and job_title not like '%Associate%'
-and job_title not like '%Assoc%'
-and job_title not like '%Advisor%'
-and job_title not like '%Workspace Product Partnerships%')))
+from stg_alumni.ucinn_ascendv2__Affiliation__c c)
 
 select e.donor_id,
 employ.status,
 employ.primary_employ_ind,
 employ.job_title,
 employ.employer,
-case when csuite.id_number is not null then 'Y' end as csuite_ind,
+case when (employ.job_title = 'EVP'
+or employ.job_title = 'Owner'
+or employ.job_title = 'Founder'
+or employ.job_title = 'Partner'
+or employ.job_title = 'Principal')
+or (employ.job_title like '%President%'
+and employ.job_title not like '%Vice%')
+or 
+(employ.job_title like '%Chief%'
+and employ.job_title not like '%Advisor%'
+and employ.job_title not like '%Assist%'
+and employ.job_title not like '%Associate%'
+and employ.job_title not like '%Aide%')
+or 
+(employ.job_title = 'Board Chair'
+or  employ.job_title = 'Board Member'
+or  employ.job_title = 'Board Dirs'
+or  employ.job_title = 'Board of Directors%'
+or  employ.job_title = 'Chairwoman'
+or employ.job_title = 'Chairman')
+---- Check Abbreviations too 
+or (employ.job_title = 'CEO'
+--- Chief Finance Officer
+or employ.job_title = 'CFO'
+--- Chief Marketing Officer
+or employ.job_title = 'CMO'
+--- Chief Information Officer
+or employ.job_title ='CIO'
+--- Chiefer Operating Office
+or employ.job_title = 'COO'
+--- Chief Tech Officer
+or employ.job_title = 'CTO'
+--- Chief Compliance officer
+or employ.job_title = 'CCO'
+--- Chief Human Resources officer
+or employ.job_title = 'CHRO'
+--- Chief Legal Officer
+or employ.job_title = 'CLO'
+--- Chief Data Officer
+or employ.job_title = 'CDO'
+--- Chief Information Security Officer
+or employ.job_title = 'CISO'
+--- Chief Strategy Officer
+or employ.job_title = 'CSO'
+)  then 'Y' end as csuite_ind,
 --- Start and End Dates
 employ.start_date,
 employ.end_date,
@@ -99,4 +83,4 @@ employ.etl_create_date,
 employ.notes
 from mv_entity e
 inner join employ on employ.id_number = e.donor_id
-left join csuite on csuite.id_number = e.donor_id
+left join l on l.donor_id = e.donor_id
