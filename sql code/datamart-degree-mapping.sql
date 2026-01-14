120fe922e93218186f3c57fdcb21294af29b826b
Paul Hively <pbhively@gmail.com>
2026-01-14 11:34:15 -0600

    Datamart degree mapping initial SQL - Datamart program, priority standardization

---
+++ sql code/datamart-degree-mapping.sql
@@ -0,0 +1,241 @@
+With
+
+alumni_base As (
+  Select
+    dc.constituent_salesforce_id
+    , kd.donor_id
+    , kd.full_name
+  From dm_alumni.dim_constituent dc
+  Inner Join mv_entity_ksm_degrees kd
+    On kd.donor_id = dc.constituent_donor_id
+)
+
+, c_degrees As (
+  Select
+    alumni_base.constituent_salesforce_id
+    , alumni_base.donor_id
+    , alumni_base.full_name
+    , deginf.name
+      As degree_record_id
+    , deginf.ap_status__c
+      As degree_status
+    , Case
+        When deginf.ucinn_ascendv2__degree_institution__c In ('001Uz00000Jp3SrIAJ', '001Uz00000NOVTpIAP') -- Northwestern University
+          Then 'Y'
+          Else 'N'
+        End
+      As nu_indicator
+    , dorg.organization_name
+      As degree_organization_name
+    , deginf.ap_school_name_formula__c
+      As degree_school_name
+    , deginf.ap_degree_type_from_degreecode__c
+      As degree_level
+/*    , Case
+        -- NU conferred year is first fallback
+        -- Reunion year is second fallback
+        When deginf.ucinn_ascendv2__conferred_degree_year__c Is Null
+          Then nvl(deginf.ap_conferred_degree_year__c, deginf.ucinn_ascendv2__reunion_year__c)
+        Else deginf.ucinn_ascendv2__conferred_degree_year__c
+        End
+      As degree_year
+    , deginf.ucinn_ascendv2__reunion_year__c
+      As degree_reunion_year
+    , deginf.ucinn_ascendv2__degree_date__c
+      As degree_grad_date
+*/    , degcd.ucinn_ascendv2__degree_code__c
+      As degree_code
+    , degcd.ucinn_ascendv2__description__c
+      As degree_name
+    , acaorg.ucinn_ascendv2__code__c
+      As department_code
+    , acaorg.ucinn_ascendv2__description_short__c
+      As department_desc
+    , acaorg.ucinn_ascendv2__description_long__c
+      As department_desc_full
+    , deginf.ap_campus__c
+      As degree_campus
+    , prog.ap_program_code__c
+      As degree_program_code
+    , prog.name
+      As degree_program
+/*    , spec.name
+      As degree_concentration_desc
+    , postcd.ap_major_code__c
+      As degree_major_code_1
+    , postcd2.ap_major_code__c
+      As degree_major_code_2
+    , postcd3.ap_major_code__c
+      As degree_major_code_3
+    , postcd.name
+      As degree_major_1
+    , postcd2.name
+      As degree_major_2
+    , postcd3.name
+      As degree_major_3
+    , deginf.ap_notes__c
+      As degree_notes
+    , trunc(deginf.etl_update_date) -- data refresh timestamp
+      As etl_update_date*/
+  -- Degree information detail
+  From stg_alumni.ucinn_ascendv2__degree_information__c deginf
+  -- Use alumni base recipe stand-in to filter
+  Inner Join alumni_base
+    On alumni_base.constituent_salesforce_id = deginf.ucinn_ascendv2__contact__c
+  Left Join dm_alumni.dim_organization dorg
+    On dorg.organization_salesforce_id = deginf.ucinn_ascendv2__degree_institution__c
+  -- Degree code
+  Left Join stg_alumni.ucinn_ascendv2__degree_code__c degcd
+    On degcd.id = deginf.ucinn_ascendv2__degree_code__c
+  -- Major codes
+  Left Join stg_alumni.ucinn_ascendv2__post_code__c postcd
+    On postcd.id = deginf.ucinn_ascendv2__post_code__c
+  Left Join stg_alumni.ucinn_ascendv2__post_code__c postcd2
+    On postcd2.id = deginf.ucinn_ascendv2__second_major_post_code__c
+  Left Join stg_alumni.ucinn_ascendv2__post_code__c postcd3
+    On postcd3.id = deginf.ap_third_major_post_code__c
+  -- Program/cohort
+  Left Join stg_alumni.ap_program__c prog
+    On prog.id = deginf.ap_program_class_section__c
+  -- Specialty code
+  Left Join stg_alumni.ucinn_ascendv2__specialty__c spec
+    On spec.id = deginf.ucinn_ascendv2__concentration_specialty__c
+  -- Academic orgs, aka department
+  Left Join stg_alumni.ucinn_ascendv2__academic_organization__c acaorg
+    On acaorg.id = deginf.ap_department__c
+)
+
+, prg As (
+Select
+  deg.donor_id
+  , deg.full_name
+  , deg.degree_record_id
+  , deg.degree_level
+  , deg.degree_status
+  , Case
+      When deg.degree_status In ('Inactive', 'Discontinued', 'Dismissed or Revoked')
+        Then '50 NON'
+      When deg.degree_status In ('Active', 'Deposited', 'Leave of Absence')
+        Or deg.degree_code Like '%-STU'
+        Then '10 STU'
+      When degree_level Like '%Doctorate%'
+        Then '01 PHD'
+      When degree_level Like '%Master%'
+        Then '02 MBA'
+      When degree_level Like '%Undergrad%'
+        Or degree_school_name = 'Undergraduate Business'
+        Then '03 BBA'
+      When degree_level Like '%Other%'
+        Then '04 OTH'
+      When degree_level Like '%Cert%'
+        Then '05 CER'
+      -- Placeholders
+      When degree_level = 'Student_placeholder'
+        Then '10 STU'
+      When degree_level = 'Nongrad_placeholder'
+        Then '50 NON'
+      When degree_level Is Null
+        Then '90 UNK'
+      Else '99 TBD'
+    End
+    As degree_level_rank
+  , deg.department_code
+  , Case
+      When deg.department_code In ('KGS2Y', '01KGS') -- New 2Y
+        And deg.department_Desc Like '%2-Year MBA%'
+        Then 'FT-2Y'
+      When deg.department_code In ('KGS1Y', '011YR')  -- New 1Y
+        And deg.department_desc Like '%1-Year MBA%'
+        Then 'FT-1Y'
+      When deg.department_code = '01MDB' -- Joint Feinberg
+        Then 'MDMBA'
+      When deg.department_code Like '01%' -- Full-time, joint full-time
+        Then substr(deg.department_code, 3)
+      When deg.department_code = '13JDM' -- Joint Law
+        Then 'JDMBA'
+      When deg.department_code = '13LCM' -- Law certificate
+        Then 'LLM'
+      When deg.department_code Like '41%' -- EMBA and IEMBA
+        Then substr(deg.department_code, 3)
+      When deg.department_code = '95BCH' -- College of Commerce 1
+        Then 'BCH'
+      When deg.department_code = '96BEV' -- College of Commerce 2
+        Then 'BEV'
+      When deg.department_code In ('AMP', 'AMPI', 'EDP', 'KSMEE')  -- KSM certificates
+        Then deg.department_code
+      When deg.department_code = '01MBI' -- Joint McCormick
+        Then 'MBAI'
+      When deg.department_code = '0000000' -- None
+        Then ''
+      Else deg.department_desc
+    End As department_desc_short
+From c_degrees deg
+  Where deg.nu_indicator = 'Y' -- Northwestern University
+    And (
+      deg.degree_school_name Like ('%Kellogg%')
+      Or deg.degree_school_name Like ('%Undergraduate Business%')
+      Or deg.degree_code = 'MBAI' -- MBAI
+      Or deg.degree_program_code In (
+        '95BCH', '96BEV' -- College of Commerce
+        , 'AMP', 'AMPI', 'EDP', 'KSMEE' -- KSMEE certificate
+      )
+    )
+)
+
+Select
+  prg.*
+  , Case
+      When degree_level_rank = '01 PHD'
+        Then 'PHD'
+      When degree_level_rank = '02 MBA'
+        Then Case
+          When department_desc_short Like '%FT-2Y%' Then 'FT-2Y'
+          When department_desc_short Like '%FT-1Y%' Then 'FT-1Y'
+          When department_desc_short Like '%JDMBA%' Then 'FT-JDMBA'
+          When department_desc_short Like '%MMM%' Then 'FT-MMM'
+          When department_desc_short Like '%MDMBA%' Then 'FT-MDMBA'
+          When department_desc_short Like '%MBAI%' Then 'FT-MBAi'
+          When department_desc_short Like '%Kellogg KEN%' Then 'FT-KENNEDY'
+          When department_desc_short Like '%Kellogg TMP%' Then 'TMP'
+          When department_desc_short Like '%Kellogg PTS%' Then 'TMP-SAT'
+          When department_desc_short Like '%Kellogg PSA%' Then 'TMP-SATXCEL'
+          When department_desc_short Like '%Kellogg PTA%' Then 'TMP-XCEL'
+          When department_desc_short Like '%Kellogg NAP%' Then 'EMP-IL'
+          When department_desc_short Like '%Kellogg WHU%' Then 'EMP-GER'
+          When department_desc_short Like '%Kellogg SCH%' Then 'EMP-CAN'
+          When department_desc_short Like '%Kellogg LAP%' Then 'EMP-FL'
+          When department_desc_short Like '%Kellogg HK%' Then 'EMP-HK'
+          When department_desc_short Like '%Kellogg JNA%' Then 'EMP-JAN'
+          When department_desc_short Like '%Kellogg RU%' Then 'EMP-ISR'
+          When department_desc_short Like '%Kellogg PKU%' Then 'EMP-CHI'
+          When department_desc_short Like '% EMP%' Then 'EMP'
+          When department_desc_short Like '%MIM%' Or department_desc_short Like '%MSMS%'
+            Then 'FT-MIM'
+          When department_desc_short Like '%MS %' Then 'FT-MS'
+          When department_desc_short Like '%MMGT%' Then 'FT-MMGT'
+          Else 'FT-UNK-MBA'
+          End
+      When degree_level_rank = '03 BBA'
+        Then Case
+          When department_desc_short Like '%BEV%' Then 'FT-EB'
+          When department_desc_short Like '%BCH%' Then 'FT-CB'
+          Else 'FT-UNK-BBA'
+          End
+      When degree_level_rank In ('04 OTH', '05 CER')
+        Then Case
+          When department_desc_short Like '%Kellogg AEP%' Then 'CERT-AEP'
+          When department_desc_short Like '%KSMEE%' Then 'EXECED'
+          When department_desc_short Like '%CERT%' Then 'EXECED'
+          When department_desc_short Like '%Institute for Mgmt%' Then 'EXECED'
+          When department_desc_short Like '%LLM%' Then 'CERT-LLM'
+          When department_desc_short Like '%Certificate%' Then 'CERT'
+          Else 'CERT-UNK'
+          End
+      When degree_level_rank = '10 STU'
+        Then 'STUDENT'
+      When degree_level_rank = '50 NON'
+        Then 'NONGRAD'
+      Else 'UNK'
+    End
+    As program
+From prg
